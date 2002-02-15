dbba7b8da7027c13faf79780b49ba3d575fe3ef0
Peter Groves <pgroves@illinois.edu>
2002-02-15 21:05:09 +0000

    *** empty log message ***

--- core/ncsa/d2k/modules/core/control/DummyOutput.java
+++ core/ncsa/d2k/modules/core/control/DummyOutput.java
@@ -3,55 +3,38 @@ package ncsa.d2k.modules.core.control;
 import ncsa.d2k.infrastructure.modules.*;
 
 /*
+	Has an output pipe that never is filled. 
+	Used to clean up unused inputs in nested itins.
 	@author pgroves
 	*/
 
-public class SaveNthObject extends ComputeModule 
+public class DummyOutput extends ComputeModule 
 	implements java.io.Serializable, HasNames{
 
 	//////////////////////
 	//d2k Props
 	////////////////////
-	boolean debug=false;	
+	
 	
 	/////////////////////////
 	/// other fields
 	////////////////////////
 
-	private Object savedObj;
-	protected int numfires;
-	protected int maxfires;
-	
+
 	//////////////////////////
 	///d2k control methods
 	///////////////////////
 
 	public boolean isReady(){
-		if(inputFlags[0]>0){
-			return true;
-		}
-		if(inputFlags[1]>0){
-			return true;
-		}
 		return false;
 	}
-
 	public void endExecution(){
 		super.endExecution();
 	}
 	public void beginExecution(){
-		reset();
 		super.beginExecution();
 	}
 	
-	private void reset(){
-		//just so it doesn't think it's done before it even starts
-		maxfires=-2;
-
-		numfires=0;
-		savedObj=null;
-	}
-		
 	/////////////////////
 	//work methods
 	////////////////////
@@ -59,27 +42,6 @@ public class SaveNthObject extends ComputeModule
 		does it
 	*/
 	public void doit() throws Exception{
-		if(inputFlags[0]>0){
-			savedObj=pullInput(0);
-			numfires++;
-			if(debug)
-				System.out.println("numfires:"+numfires);
-
-		}
-		if(inputFlags[1]>0){
-			maxfires=((Integer)pullInput(1)).intValue();
-			if(debug)
-				System.out.println("maxfires set to "+maxfires);
-			return;
-		}
-		
-		if(numfires==maxfires){
-			if(savedObj!=null){
-				pushOutput(savedObj, 0);
-				reset();
-			}else
-				return;
-		}
 	}
 		
 	
@@ -89,14 +51,15 @@ public class SaveNthObject extends ComputeModule
 
 
 	public String getModuleInfo(){
-		return "";
+		return "	Has an output pipe that never is filled. "+
+		"Used to clean up unused inputs in nested itins.";
 	}
 	
    	public String getModuleName() {
-		return "";
+		return "Nested Input Cleanup";
 	}
 	public String[] getInputTypes(){
-		String[] s= {"java.lang.Object","java.lang.Integer"};
+		String[] s= {};
 		return s;
 	}
 
@@ -104,11 +67,7 @@ public class SaveNthObject extends ComputeModule
 		switch (index){
 			case(0): {
 				return "";
-			}case(1): {
-				return "";
 			}
-
-
 			default:{
 				return "No such input.";
 			}
@@ -118,9 +77,8 @@ public class SaveNthObject extends ComputeModule
 	public String getInputName(int index) {
 		switch (index){
 			case(0): {
-				return "Saved Object";
+				return "";
 			}
-			case(1):{return "Object Count";}
 			default:{
 				return "No such input.";
 			}
@@ -134,7 +92,7 @@ public class SaveNthObject extends ComputeModule
 	public String getOutputInfo(int index){
 		switch (index){
 			case(0): {
-				return "";
+				return "This pipe will never be filled";
 			}
 			default:{
 				return "No such output.";
@@ -144,7 +102,7 @@ public class SaveNthObject extends ComputeModule
 	public String getOutputName(int index) {
 		switch (index){
 			case(0): {
-				return "The Nth object";
+				return "Dummy Pipe";
 			}
 			default:{
 				return "No such output.";
@@ -154,12 +112,7 @@ public class SaveNthObject extends ComputeModule
 	////////////////////////////////
 	//D2K Property get/set methods
 	///////////////////////////////
-	public boolean getDebug(){
-		return debug;
-	}
-	public void setDebug(boolean b){
-		debug=b;
-	}
+
 	/*
 	public boolean get(){
 		return ;
--- core/ncsa/d2k/modules/core/control/ObjectBlocker.java
+++ core/ncsa/d2k/modules/core/control/ObjectBlocker.java
@@ -3,55 +3,40 @@ package ncsa.d2k.modules.core.control;
 import ncsa.d2k.infrastructure.modules.*;
 
 /*
+	Takes in an object, does nothing with it.
+	Used to clean up unused outputs in nested
+	itins
+	
 	@author pgroves
 	*/
 
-public class SaveNthObject extends ComputeModule 
+public class ObjectBlocker extends ComputeModule 
 	implements java.io.Serializable, HasNames{
 
 	//////////////////////
 	//d2k Props
 	////////////////////
-	boolean debug=false;	
+	
 	
 	/////////////////////////
 	/// other fields
 	////////////////////////
 
-	private Object savedObj;
-	protected int numfires;
-	protected int maxfires;
-	
+
 	//////////////////////////
 	///d2k control methods
 	///////////////////////
 
 	public boolean isReady(){
-		if(inputFlags[0]>0){
-			return true;
-		}
-		if(inputFlags[1]>0){
-			return true;
-		}
-		return false;
+		return super.isReady();
 	}
-
 	public void endExecution(){
 		super.endExecution();
 	}
 	public void beginExecution(){
-		reset();
 		super.beginExecution();
 	}
 	
-	private void reset(){
-		//just so it doesn't think it's done before it even starts
-		maxfires=-2;
-
-		numfires=0;
-		savedObj=null;
-	}
-		
 	/////////////////////
 	//work methods
 	////////////////////
@@ -59,27 +44,7 @@ public class SaveNthObject extends ComputeModule
 		does it
 	*/
 	public void doit() throws Exception{
-		if(inputFlags[0]>0){
-			savedObj=pullInput(0);
-			numfires++;
-			if(debug)
-				System.out.println("numfires:"+numfires);
-
-		}
-		if(inputFlags[1]>0){
-			maxfires=((Integer)pullInput(1)).intValue();
-			if(debug)
-				System.out.println("maxfires set to "+maxfires);
-			return;
-		}
-		
-		if(numfires==maxfires){
-			if(savedObj!=null){
-				pushOutput(savedObj, 0);
-				reset();
-			}else
-				return;
-		}
+		pullInput(0);
 	}
 		
 	
@@ -89,14 +54,15 @@ public class SaveNthObject extends ComputeModule
 
 
 	public String getModuleInfo(){
-		return "";
+		return "Takes in an object, does nothing with it."+
+				"Used to clean up unused outputs in nested itins ";
 	}
 	
    	public String getModuleName() {
 		return "";
 	}
 	public String[] getInputTypes(){
-		String[] s= {"java.lang.Object","java.lang.Integer"};
+		String[] s= {"java.lang.Object"};
 		return s;
 	}
 
@@ -104,11 +70,7 @@ public class SaveNthObject extends ComputeModule
 		switch (index){
 			case(0): {
 				return "";
-			}case(1): {
-				return "";
 			}
-
-
 			default:{
 				return "No such input.";
 			}
@@ -118,16 +80,15 @@ public class SaveNthObject extends ComputeModule
 	public String getInputName(int index) {
 		switch (index){
 			case(0): {
-				return "Saved Object";
+				return "Unwanted Object";
 			}
-			case(1):{return "Object Count";}
 			default:{
 				return "No such input.";
 			}
 		}
 	}
 	public String[] getOutputTypes(){
-		String[] s={"java.lang.Object"};
+		String[] s={};
 		return s;
 	}
 
@@ -144,7 +105,7 @@ public class SaveNthObject extends ComputeModule
 	public String getOutputName(int index) {
 		switch (index){
 			case(0): {
-				return "The Nth object";
+				return "";
 			}
 			default:{
 				return "No such output.";
@@ -154,12 +115,7 @@ public class SaveNthObject extends ComputeModule
 	////////////////////////////////
 	//D2K Property get/set methods
 	///////////////////////////////
-	public boolean getDebug(){
-		return debug;
-	}
-	public void setDebug(boolean b){
-		debug=b;
-	}
+
 	/*
 	public boolean get(){
 		return ;
