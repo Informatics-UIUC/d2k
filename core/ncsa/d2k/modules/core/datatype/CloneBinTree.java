180a0bd78444890aa69047bdb47e4f94ad2e737e
David Clutter <clutter@illinois.edu>
2002-03-07 16:54:21 +0000

    *** empty log message ***

--- core/ncsa/d2k/modules/core/datatype/CloneBinTree.java
+++ core/ncsa/d2k/modules/core/datatype/CloneBinTree.java
@@ -1,12 +1,15 @@
-package ncsa.d2k.modules.core.prediction.naivebayes;
+package ncsa.d2k.modules.core.datatype;
 
 import ncsa.d2k.infrastructure.modules.*;
-import ncsa.d2k.modules.core.datatype.BinTree;
 
+
+/**
+ * Clones a BinTree.
+ */
 public class CloneBinTree extends DataPrepModule {
 
 	public String getModuleInfo() {
-		return "";
+		return "Clones a BinTree N times.";
 	}
 
 	public String[] getInputTypes() {
@@ -22,19 +25,28 @@ public class CloneBinTree extends DataPrepModule {
 	}
 
 	public String getInputInfo(int i) {
-		return "";
+		if(i == 0)
+			return "The BinTree to clone.";
+		else
+			return "The number of times to clone the bin tree.";
 	}
 
 	public String getOutputInfo(int i) {
-		return "";
+		if(i == 0)
+			return "A cloned BinTree.";
+		else
+			return "The number of times to clone the bin tree.";
 	}
 
 	public void doit() {
+		// pull in the inputs
 		BinTree bt = (BinTree)pullInput(0);
 		Integer n = (Integer)pullInput(1);
 
+		// push N along
 		pushOutput(n, 1);
 
+		// push out N clones.
 		int val = n.intValue();
 		for(int i = 0; i < val; i++) {
 			try {
