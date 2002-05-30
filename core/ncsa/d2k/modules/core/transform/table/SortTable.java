9e363ef78bff9c981695d8f61ffcec41aba5c248
David Clutter <clutter@illinois.edu>
2002-05-30 22:01:35 +0000

    *** empty log message ***

--- core/ncsa/d2k/modules/core/transform/table/SortTable.java
+++ core/ncsa/d2k/modules/core/transform/table/SortTable.java
@@ -12,197 +12,259 @@ import java.awt.event.*;
 import javax.swing.*;
 
 import ncsa.d2k.modules.core.datatype.table.*;
-import ncsa.d2k.modules.core.datatype.table.basic.*;
-/**
-	MultipleSort.java
 
-	Bradley Berkin
-	5/01
+/**
+ * Perform a cascading Excel-style sort on a MutableTable.
 */
-public class SortTable extends ncsa.d2k.infrastructure.modules.UIModule
-{
+public class SortTable extends ncsa.d2k.infrastructure.modules.UIModule {
 
 	public String getInputInfo(int index) {
-		switch (index) {
-			case 0: return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><D2K>  <Info common=\"unsorted\">    <Text>Unsorted table </Text>  </Info></D2K>";
-			default: return "No such input";
-		}
+		return "";
 	}
 
 	public String[] getInputTypes() {
-		String[] types = {"ncsa.d2k.modules.core.datatype.table.basic.TableImpl"};
+		String[] types = {"ncsa.d2k.modules.core.datatype.table.MutableTable"};
 		return types;
 	}
 
 	public String getOutputInfo(int index) {
-		switch (index) {
-			case 0: return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><D2K>  <Info common=\"sorted\">    <Text>Sorted table </Text>  </Info></D2K>";
-			default: return "No such output";
-		}
+		return "";
 	}
 
 	public String[] getOutputTypes() {
-		String[] types = {"ncsa.d2k.modules.core.datatype.table.basic.TableImpl"};
+		String[] types = {"ncsa.d2k.modules.core.datatype.table.MutableTable"};
 		return types;
 	}
 
 	public String getModuleInfo() {
-		return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><D2K>  <Info common=\"MultipleSort\">    <Text>Sorts the rows of the table by the first specified column. Finds equivalent rows and sorts by the second specified column. Repeats for specified number of columns. </Text>  </Info></D2K>";
+		return "Sort the columns of a Table.";
 	}
 
 	protected UserView createUserView() {
-		return new SortView();
+		return new SortTableView();
 	}
 
 	public String[] getFieldNameMapping() {
 		return null;
-
 	}
 
-	int numsort = 5;
+	private int numberOfSorts = 5;
 
-	public int getNumsort() {
-		return numsort;
+	public int getNumberOfSorts() {
+		return numberOfSorts;
 	}
 
-	public void setNumsort(int sort) {
-		numsort = sort;
+	public void setNumberOfSorts(int sort) {
+		numberOfSorts = sort;
 	}
 
-	public void Done(Table table) {
+	public void done(Table table) {
 		pushOutput(table, 0);
 		viewDone("Done");
 	}
 
-	boolean reorder = false;
+	boolean reorderColumns = false;
 
-	public boolean getReorder() {
-		return reorder;
+	public boolean getReorderColumns() {
+		return reorderColumns;
 	}
 
-	public void setReorder(boolean order) {
-		reorder = order;
+	public void setReorderColumns(boolean order) {
+		reorderColumns = order;
 	}
 
-}
+	private static final String NONE = "None";
 
+	/**
+		SortTableView
+	*/
+	private class SortTableView extends ncsa.d2k.controller.userviews.swing.JUserPane implements ActionListener {
 
-/**
-	SortView
-*/
-class SortView extends ncsa.d2k.controller.userviews.swing.JUserPane implements ActionListener {
-	SortTable module;
-
-	TableImpl table;
-	int columns;
-	int rows;
-	int numsort;
-
-	int[] sortorder;
-	int sortsize;
-
+		MutableTable table;
+		int columns;
+		int rows;
+		int numsort;
+		int[] sortorder;
+		int sortsize;
+/*
 	QuickQueue queue, lastqueue;
+*/
+		JLabel[] sortlabels;
+		JComboBox[] sortchoices;
 
-	JLabel[] sortlabels;
-	JComboBox[] sortchoices;
+		JButton done, abort;
 
-	JButton done, abort;
-
-	public void initView(ViewModule viewmod) {
-		module = (SortTable) viewmod;
-	}
-
-	public void setInput(Object object, int inputindex) {
-		table = (TableImpl) object;
-		columns = table.getNumColumns();
-		rows = table.getNumRows();
-		numsort = module.getNumsort();
-
-		if (numsort > columns)
-			numsort = columns;
-
-		sortlabels = new JLabel[numsort];
-		for(int index=0; index < numsort; index++) {
-			JLabel label = new JLabel((index+1) + ". Sort by: ");
-			sortlabels[index] = label;
+		public void initView(ViewModule viewmod) {
+	//		module = (SortTable) viewmod;
 		}
 
-		String[] columnlabels = new String[columns+1];
-		columnlabels[0] = "none";
-		for(int index=0; index < columns; index++) {
-			String columnlabel = table.getColumnLabel(index);
-			columnlabels[index+1] = columnlabel;
+		public void setInput(Object object, int inputindex) {
+			table = (MutableTable) object;
+			columns = table.getNumColumns();
+			rows = table.getNumRows();
+			numsort = getNumberOfSorts();
+
+			if (numsort > columns)
+				numsort = columns;
+
+			sortlabels = new JLabel[numsort];
+			for(int index=0; index < numsort; index++) {
+				JLabel label = new JLabel((index+1) + ". Sort by: ");
+				sortlabels[index] = label;
+			}
+
+			String[] columnlabels = new String[columns+1];
+			columnlabels[0] = NONE;
+			for(int index=0; index < columns; index++) {
+				String columnlabel = table.getColumnLabel(index);
+				columnlabels[index+1] = columnlabel;
+			}
+
+			sortchoices = new JComboBox[numsort];
+			for(int index=0; index < numsort; index++) {
+				JComboBox combobox = new JComboBox(columnlabels);
+				sortchoices[index] = combobox;
+			}
+
+			buildView();
 		}
 
-		sortchoices = new JComboBox[numsort];
-		for(int index=0; index < numsort; index++) {
-			JComboBox combobox = new JComboBox(columnlabels);
-			sortchoices[index] = combobox;
+		public void buildView() {
+			removeAll();
+
+			setLayout(new BorderLayout());
+
+			JPanel scrollpanel = new JPanel();
+			scrollpanel.setLayout(new GridBagLayout());
+
+			for(int index=0; index < numsort; index++) {
+				Constrain.setConstraints(scrollpanel, sortlabels[index], 0, index, 1, 1, GridBagConstraints.NONE,
+					GridBagConstraints.WEST, 0, 1);
+				Constrain.setConstraints(scrollpanel, sortchoices[index], 1, index, 1, 1, GridBagConstraints.HORIZONTAL,
+					GridBagConstraints.WEST, 1, 0);
+			}
+
+			JScrollPane scrollpane = new JScrollPane(scrollpanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+			add(scrollpane, BorderLayout.CENTER);
+
+			JPanel buttonpanel = new JPanel();
+			abort = new JButton("Abort");
+			done = new JButton("Done");
+			abort.addActionListener(this);
+			done.addActionListener(this);
+			buttonpanel.add(abort);
+			buttonpanel.add(done);
+			add(buttonpanel, BorderLayout.SOUTH);
 		}
 
-		buildView();
-	}
-
-	public void buildView() {
-		removeAll();
-
-		setLayout(new BorderLayout());
-
-		JPanel scrollpanel = new JPanel();
-		scrollpanel.setLayout(new GridBagLayout());
-
-		for(int index=0; index < numsort; index++) {
-			Constrain.setConstraints(scrollpanel, sortlabels[index], 0, index, 1, 1, GridBagConstraints.NONE,
-				GridBagConstraints.WEST, 0, 1);
-			Constrain.setConstraints(scrollpanel, sortchoices[index], 1, index, 1, 1, GridBagConstraints.HORIZONTAL,
-				GridBagConstraints.WEST, 1, 0);
+		public Dimension getPreferredSize() {
+			return new Dimension(300, 200);
 		}
 
-		JScrollPane scrollpane = new JScrollPane(scrollpanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-			ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		add(scrollpane, BorderLayout.CENTER);
-
-		JPanel buttonpanel = new JPanel();
-		abort = new JButton("Abort");
-		done = new JButton("Done");
-		abort.addActionListener(this);
-		done.addActionListener(this);
-		buttonpanel.add(abort);
-		buttonpanel.add(done);
-		add(buttonpanel, BorderLayout.SOUTH);
-	}
-
-	public Dimension getPreferredSize() {
-		return new Dimension(300, 200);
-	}
-
-	public void getSortOrder() {
-		sortorder = new int[numsort];
-		initialize(sortorder);
-		int choicesindex = 0;
-		int index = sortchoices[choicesindex].getSelectedIndex();
-		while ((index != 0) && (choicesindex < numsort)) {
-			sortorder[choicesindex] = index-1;
-			choicesindex++;
-			if (choicesindex < numsort)
-				index = sortchoices[choicesindex].getSelectedIndex();
+		public void getSortOrder() {
+			/*sortorder = new int[numsort];
+			initialize(sortorder);
+			int choicesindex = 0;
+			int index = sortchoices[choicesindex].getSelectedIndex();
+			while ((index != 0) && (choicesindex < numsort)) {
+				sortorder[choicesindex] = index-1;
+				choicesindex++;
+				if (choicesindex < numsort)
+					index = sortchoices[choicesindex].getSelectedIndex();
+			}
+			sortsize = choicesindex;
+			*/
+			ArrayList sortList = new ArrayList();
+			for(int i = 0; i < sortchoices.length; i++) {
+				int idx = sortchoices[i].getSelectedIndex();
+				if(idx != 0)
+					sortList.add(new Integer(idx-1));
+				else
+					break;
+			}
+			sortorder = new int[sortList.size()];
+			for(int i = 0; i < sortList.size(); i++)
+				sortorder[i] = ((Integer)sortList.get(i)).intValue();
+			numsort = sortorder.length;
 		}
-		sortsize = choicesindex;
-	}
 
-	public void actionPerformed(ActionEvent event) {
-		Object source = event.getSource();
-		if (source == done) {
-			getSortOrder();
-			sort();
+		public void actionPerformed(ActionEvent event) {
+			Object source = event.getSource();
+			if (source == done) {
+				getSortOrder();
+				sort();
+			}
+			if (source == abort)
+				viewAbort();
 		}
-		if (source == abort)
-			module.viewAbort();
-	}
 
+		/**
+		 * Find the indices where a run ends in a column.  A run is a block of
+		 * equal values in the column.  The column is assumed to be sorted.
+		 * This will return an array whose values are the indices of the
+		 * rows that end a run.
+		 * @param col the column of interest
+		 * @return
+		 */
+		private int[] findRuns(int col) {
+			//System.out.println("FindRuns: "+table.getColumnLabel(col));
+			ArrayList runList = new ArrayList();
+			if(table.isColumnScalar(col)) {
+				double currentVal = table.getDouble(0, col);
+				for(int i = 1; i < table.getNumRows(); i++) {
+					double rowVal = table.getDouble(i, col);
+					if(rowVal != currentVal) {
+						runList.add(new Integer(i-1));
+						currentVal = rowVal;
+					}
+				}
+			}
+			else {
+				String currentVal = table.getString(0, col);
+				for(int i = 1; i < table.getNumRows(); i++) {
+					String rowVal = table.getString(i, col);
+					if(!rowVal.equals(currentVal)) {
+						runList.add(new Integer(i-1));
+						currentVal = rowVal;
+					}
+				}
+			}
+			runList.add(new Integer(table.getNumRows()-1));
+			int[] retVal = new int[runList.size()];
+			for(int i = 0; i < retVal.length; i++) {
+				retVal[i] = ((Integer)runList.get(i)).intValue();
+				//System.out.println("Run["+i+"]: "+retVal[i]);
+			}
+			return retVal;
+		}
 
-	public void sort() {
-		Column column;
+		/**
+		 * Sort the table for each column selected.
+		 */
+		private void sort() {
+			if(sortorder.length > 0) {
+				table.sortByColumn(sortorder[0]);
+				for(int i = 1; i < sortorder.length; i++) {
+					// now, find the runs in the (i-1)th column and do a
+					// table.sortByColumn(col, begin, end) for each run
+					int[] runs = findRuns(sortorder[i-1]);
+
+					// do the first sort outside the loop
+					table.sortByColumn(sortorder[i], 0, runs[0]);
+					for(int j = 1; j < runs.length; j++) {
+						// now sort from the end of the last run to the end
+						// of the current run
+						table.sortByColumn(sortorder[i], runs[j-1]+1, runs[j]);
+					}
+				}
+			}
+			if(getReorderColumns())
+				reorder();
+			pushOutput(table, 0);
+			viewDone("Done");
+		}
+/*		Column column;
 		int[] sorted;
 		int sortindex = 0;
 
@@ -235,13 +297,13 @@ class SortView extends ncsa.d2k.controller.userviews.swing.JUserPane implements 
 			}
 
 		}
-		module.Done(table);
-	}
+		module.done(table);
+*/
 
 	// Reorder columns
 	public void reorder() {
 		//Column[] internal = (Column[]) table.getInternal();
-		Column[] internal = new Column[table.getNumColumns()];
+/*		Column[] internal = new Column[table.getNumColumns()];
 		for(int i = 0; i < internal.length; i++)
 			internal[i] = table.getColumn(i);
 
@@ -267,9 +329,14 @@ class SortView extends ncsa.d2k.controller.userviews.swing.JUserPane implements 
 		//table.setInternal(ordered);
 		for(int i = 0; i < ordered.length; i++)
 			table.setColumn(ordered[i], i);
+*/
+		for(int i = 0; i < sortorder.length; i++) {
+			table.swapColumns(sortorder[i], i);
+		}
+	}
 	}
 
-	public void firstCollect(Column column, int[] sorted) {
+/*	public void firstCollect(Column column, int[] sorted) {
 		int previous = 0;
 		int current = 0;
 		int compare = 0;
@@ -289,8 +356,9 @@ class SortView extends ncsa.d2k.controller.userviews.swing.JUserPane implements 
 			queue.push(new Run(previous, current-1));
 		}
 	}
+	*/
 
-	public void collect(Column column, int[] sorted) {
+/*	public void collect(Column column, int[] sorted) {
 		int previous = 0;
 		int current = 0;
 		int compare = 0;
@@ -315,13 +383,14 @@ class SortView extends ncsa.d2k.controller.userviews.swing.JUserPane implements 
 			}
 		}
 	}
+*/
 
 	public void initialize(int[] array) {
 		for (int index=0; index < array.length; index++)
 			array[index] = index;
 	}
 
-	public void mergesort(Column column, int[] sorted, int start, int end) {
+/*	public void mergesort(Column column, int[] sorted, int start, int end) {
 		if (start < end) {
 			int middle = (start+end)/2;
 			mergesort(column, sorted, start, middle);
@@ -390,5 +459,5 @@ class SortView extends ncsa.d2k.controller.userviews.swing.JUserPane implements 
 			System.out.println(start + "-" + end);
 		}
 	}
+*/
 }
-
