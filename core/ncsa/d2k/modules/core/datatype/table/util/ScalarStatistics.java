859da76f039aed97ca931c74e52d1a189a0600eb
Bradley Berkin <berkin@illinois.edu>
2002-10-29 23:38:42 +0000

    *** empty log message ***

--- core/ncsa/d2k/modules/core/datatype/table/util/ScalarStatistics.java
+++ core/ncsa/d2k/modules/core/datatype/table/util/ScalarStatistics.java
@@ -11,76 +11,112 @@ package ncsa.d2k.modules.core.datatype.table.util;
  */
 public class ScalarStatistics {
 
-	double mean;
-	double median;
-	double variance;
-	double standardDeviation;
-	double minimum;
-	double maximum;
+  double mean;
+  double median;
+  double variance;
+  double standardDeviation;
+  double firstquartile;
+  double thirdquartile;
+  double minimum;
+  double maximum;
 
-	/**
-	 * Construct a new ScalarStatistics
-	 * @param ave
-	 * @param med
-	 * @param var
-	 * @param sd
-	 * @param min
-	 * @param max
-	 */
-	ScalarStatistics(double ave, double med, double var, double sd, double min, double max) {
-		mean = ave;
-		median = med;
-		variance = var;
-		standardDeviation = sd;
-		minimum = min;
-		maximum = max;
-	}
+  /**
+   * Construct a new ScalarStatistics
+   * @param ave
+   * @param med
+   * @param var
+   * @param sd
+   * @param min
+   * @param max
+   */
+  ScalarStatistics(double ave, double med, double var, double sd, double min, double max) {
+    mean = ave;
+    median = med;
+    variance = var;
+    standardDeviation = sd;
+    minimum = min;
+    maximum = max;
+  }
 
-	/**
-	 * Get the mean.
-	 * @return the mean
-	 */
-	public double getMean() {
-		return mean;
-	}
+  /**
+   * Construct a new ScalarStatistics
+   * @param ave
+   * @param med
+   * @param var
+   * @param sd
+   * @param min
+   * @param max
+   * @param fq
+   * @param tq
+   */
+  ScalarStatistics(double ave, double med, double var, double sd, double min, double max, double fq, double tq) {
+    this(ave, med, var, sd, min, max);
 
-	/**
-	 * Get the median.
-	 * @return the median
-	 */
-	public double getMedian() {
-		return median;
-	}
+    firstquartile = fq;
+    thirdquartile = tq;
+  }
 
-	/**
-	 * Get the variance.
-	 * @return the variance
-	 */
-	public double getVariance() {
-		return variance;
-	}
+  /**
+   * Get the mean.
+   * @return the mean
+   */
+  public double getMean() {
+    return mean;
+  }
 
-	/**
-	 * Get the standard deviation
-	 * @return the standard deviation
-	 */
-	public double getStandardDeviation() {
-		return standardDeviation;
-	}
+  /**
+   * Get the median.
+   * @return the median
+   */
+  public double getMedian() {
+    return median;
+  }
 
-	/**
-	 * Get the minimum.
-	 * @return the minimum
-	 */
-	public double getMinimum() {
-		return minimum;
-	}
+  /**
+   * Get the variance.
+   * @return the variance
+   */
+  public double getVariance() {
+    return variance;
+  }
 
-	/**
-	 * Get the maximum
-	 * @return the maximum
-	 */
-	public double getMaximum() {
-		return maximum;
-	}
+  /**
+   * Get the standard deviation
+   * @return the standard deviation
+   */
+  public double getStandardDeviation() {
+    return standardDeviation;
+  }
+
+  /**
+   * Get the minimum.
+   * @return the minimum
+   */
+  public double getMinimum() {
+    return minimum;
+  }
+
+  /**
+   * Get the maximum
+   * @return the maximum
+   */
+  public double getMaximum() {
+    return maximum;
+  }
+
+  /**
+   * Get the first quartile
+   * @return the first quartile
+   */
+  public double getFirstQuartile() {
+    return firstquartile;
+  }
+
+  /**
+   * Get the third quartile
+   * @return the third quartile
+   */
+  public double getThirdQuartile() {
+    return thirdquartile;
+  }
 }
