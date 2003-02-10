bb33e19228cd0af17cb8616412d27a52d51ad916
Unknown <unknown@illinois.edu>
2003-02-10 17:25:17 +0000

    This commit was manufactured by cvs2svn to create branch 'basic'.

--- core/ncsa/d2k/modules/core/datatype/ExpressionListener.java
+++
@@ -1,7 +0,0 @@
-package ncsa.d2k.modules.core.datatype;
-
-public interface ExpressionListener {
-
-   public void expressionChanged(Object evaluation);
-
-}
--- core/ncsa/d2k/modules/core/io/dstp/ProgressQueryable.java
+++
@@ -1,5 +0,0 @@
-package ncsa.d2k.modules.core.io.dstp;
-
-public interface ProgressQueryable {
-  public int getProgress();
-}
