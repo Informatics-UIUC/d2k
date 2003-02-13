a7b8c2bd2736bb2cd67bc808d1fd7d0af1acb834
Ruth Aydt <aydt@illinois.edu>
2003-02-13 02:01:49 +0000

    1) Added more extensive docs, including java docs 2) deleted code that was commented out 3) added exception handling (perhaps needs better still) 4) attempted to change so properties wouldn't show since not editable -    didn't work;  still needs help

--- core/ncsa/d2k/modules/core/io/file/input/InputSerializedObject.java
+++ core/ncsa/d2k/modules/core/io/file/input/InputSerializedObject.java
@@ -10,30 +10,37 @@ import ncsa.d2k.core.modules.*;
 public class InputSerializedObject extends InputModule {
 
     /**
-     * put your documentation comment here
-     * @return
+     * Return the common name of this module.
+     * @reutrn The display name for this module.
      */
-  /*public String getModuleInfo () {
-  return "<html>  <head>      </head>  <body>    Reads a serialized object from a file PROPS-usePropFileName is true if you     don't want it to wait for an input for a filename and instead use the     filename in the properties  </body></html>";
- }*/
-
     public String getModuleName() {
         return "Read a Serialized Object from a File";
     }
 
     /**
-     * put your documentation comment here
-     * @return
+     * Return information about the module
+     * @return A detailed description of the module.
      */
     public String getModuleInfo () {
-        StringBuffer sb = new StringBuffer("<p>Overview: ");
-        sb.append("This module will read a serialized object from a file.");
-        return sb.toString();
+      StringBuffer sb = new StringBuffer("<p>Overview: ");
+      sb.append("This module reads a Java serialized object from a file.");
+      sb.append("</p><p>Detailed Description: In Java, an object can be ");
+      sb.append("converted to a stream of bytes and written out to a file. ");
+      sb.append("in a process called <em>serialization</em>.  ");
+      sb.append("</p><p>This module opens the file indicated by the <i>File ");
+      sb.append("Name</i> input port and reads the file to reload the Java ");
+      sb.append("object that was serialized to the file.  The resulting ");
+      sb.append("object is made available on the <i>Java Object</i> output ");
+      sb.append("port.");
+      sb.append("</p><p>The module will exit with an error if the file does ");
+      sb.append("not exist or does not contain a serialized object. </p> ");
+
+      return sb.toString();
     }
 
     /**
-     * put your documentation comment here
-     * @return
+     * Return a String array containing datatypes of the inputs to this module
+     * @return The datatypes of the module inputs.
      */
     public String[] getInputTypes () {
         String[] types = {"java.lang.String"};
@@ -41,8 +48,8 @@ public class InputSerializedObject extends InputModule {
     }
 
     /**
-     * put your documentation comment here
-     * @return
+     * Returns a String array containing datatypes of the output to this module
+     * @return The datatypes of the module outputs.
      */
     public String[] getOutputTypes () {
         String[] types = {"java.lang.Object"};
@@ -50,126 +57,100 @@ public class InputSerializedObject extends InputModule {
     }
 
     /**
-     * put your documentation comment here
-     * @param i
-     * @return
+     * Return a description of the specified input port.
+     * @param i The index of the input.
+     * @return The description of the input.
      */
     public String getInputInfo (int i) {
         switch (i) {
-            case 0: return "The filename of the serialized Object to read in.";
+            case 0: return "The filename of the serialized object to be read.";
             default: return "No such input";
         }
     }
 
     /**
-     * put your documentation comment here
-     * @param i
-     * @return
+     * Return the name of a specific input.
+     * @param i The index of the input.
+     * @return The name of the input.
      */
     public String getInputName (int i) {
         switch(i) {
             case 0:
                 return "File Name";
-            default: return "NO SUCH INPUT!";
+            default: return "No such input";
         }
     }
 
     /**
-     * put your documentation comment here
-     * @param i
-     * @return
+     * Return the name of a specific output.
+     * @param i The index of the output.
+     * @return The name of the output.
      */
     public String getOutputInfo (int i) {
         switch (i) {
-            case 0: return "The Object that was read in.";
+            case 0: return "The Java Object that was read from the file.";
             default: return "No such output";
         }
     }
 
     /**
-     * put your documentation comment here
-     * @param i
-     * @return
+     * Return the name of a specified output.
+     * @param i The index of the output.
+     * @return The name of the output.
      */
     public String getOutputName (int i) {
         switch(i) {
             case 0:
-                return "Deserialized Object";
-            default: return "NO SUCH OUTPUT!";
+                return "Java Object";
+            default: return "No such output";
         }
     }
-    ////////////////
-    // Properties //
-    ////////////////
-/*  private String FileName;      // = "ObjectFile.ser";
 
- /**
-  * put your documentation comment here
-  * @param value
-  */
-/*  public void setFileName (String value) {
-    this.FileName = value;
-  }
-
-  /**
-   * put your documentation comment here
-   * @return
-   */
-/*  public String getFileName () {
-    return  this.FileName;
-  }
-  private boolean usePropFileName = false;
-
-  /**
-   * put your documentation comment here
-   * @param b
-   */
-/*  public void setUsePropFileName (boolean b) {
-    usePropFileName = b;
-  }
-
-  /**
-   * put your documentation comment here
-   * @return
-   */
-/*  public boolean getUsePropFileName () {
-    return  usePropFileName;
-  }
-  */
-
- //////////////////
- //isReady
- /////////////////
-  /*public boolean isReady () {
-    if (usePropFileName) {
-      return  true;
+   /** Return an array with information on the properties the user may update.
+    *  Empty for this module!
+    *  @return The PropertyDescriptions for properties the user may update. (none)
+    */
+    public PropertyDescription[] getPropertiesDescriptions() {
+        PropertyDescription[] pds = null;
+        return pds;
     }
-    else {
-      return  super.isReady();
-    }
-  }*/
 
  //////////
  // Doit //
  //////////
     public void doit () throws Exception {
-        //    String FileName = null;
-        //try {
-        //if (!usePropFileName) {
+
         String FileName = (String)(pullInput(0));
-        //}
-        FileInputStream file = new FileInputStream(FileName);
-        ObjectInputStream in = new ObjectInputStream(file);
+        FileInputStream file = null;
+
+        try {
+           file = new FileInputStream(FileName);
+        }
+        catch (IOException e) {
+           throw new IOException( "Could not open file: " + FileName +
+                                  "\n" + e );
+        }
+
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(file);
+        }
+        catch (IOException e) {
+           throw new IOException( "File " + FileName + " has invalid format" +
+                                  "\n" + e );
+        }
+
         Object object = null;
-        //try {
-        object = (Object)in.readObject();
-        //} catch (java.lang.ClassNotFoundException IOE) {
-        //  System.out.println("java.lang.ClassNotFoundException " + IOE);
-        // }
+        try {
+           object = (Object)in.readObject();
+        }
+        catch( java.lang.ClassNotFoundException e ) {
+           throw new ClassNotFoundException( "Unable to deserialize object." +
+                                             "\n" + e );
+        }
+
         in.close();
         pushOutput(object, 0);
-        //} catch (java.io.IOException IOE) {
-        //  System.out.println("IOException!!! could not open " + FileName);
-        //}
+
     }
 }
