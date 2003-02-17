c760dc1754e864f35a19273c86cf9fa9b1e8c91d
David Tcheng <dtcheng@illinois.edu>
2003-02-17 00:36:33 +0000

    renamed directory space to parameter

---
+++ core/ncsa/d2k/modules/core/datatype/parameter/ParameterPoint.java
@@ -0,0 +1,91 @@
+package ncsa.d2k.modules.core.datatype.space;
+
+import ncsa.d2k.modules.core.datatype.table.*;
+
+/**
+
+ParameterPoint:
+
+The ParameterPoint object can extend ExampleImpl.  It will be input to the learning algorithm.  It should use the same column names and column order as the ParameterSpace implementation.
+
+Note: It is important that this be an Example so that additional layers (optimizing the optimizer etc..) can more easily be implemented.
+
+*/
+
+
+public interface ParameterPoint extends Example, java.io.Serializable {
+
+  //static final long serialVersionUID = 2508941379956505568L;
+
+  //static final long serialVersionUID = -8081185634463160785L;
+
+
+  /**
+   * Get the number of parameters that define the space.
+   * @return an int value representing the minimum possible value of the parameter.
+   */
+  public int getNumParameters();
+
+  /**
+   * Get the name of a parameter.
+   * @param parameterIndex the index of the parameter of interest.
+   * @return a string value representing the name of the parameter.
+   */
+  public String getParameterName(int parameterIndex);
+
+  /**
+   * Get the names of all the parameters.
+   * @return an array of string values representing the names of the all the parameters.
+   */
+  public String [] getParameterNames();
+
+  /**
+   * Get the resolution of a parameter.
+   * @param parameterIndex the index of the parameter of interest.
+   * @return a int value representing the number of intervals between the min and max parameter values.
+   */
+  public int getResolution(int parameterIndex);
+
+  /**
+   * Get the subspace index of a parameter.
+   * @param parameterIndex the index of the parameter of interest.
+   * @return a int value representing the subpace index number of parameter.
+   */
+  public int getSubspaceIndex(int parameterIndex);
+
+  /**
+   * Get the number of subspaces that defines the space.
+   * @return a int value representing the number subspaces that define the space.
+   */
+  public int getNumSubspaces();
+
+  /**
+   * Get the number of parameters in each subspace.
+   * @return a int array of values the number of parameters defining each subspace.
+   */
+  public int [] getSubspaceNumParameters();
+
+  /**
+   * Get a subspace from the space.
+   * @param subspaceIndex the index of the subspace of interest.
+   * @return a ParameterPoint which defines the indicated subspace.
+   */
+  public ParameterPoint getSubspace(int subspaceIndex);
+
+  /**
+   * Join two ParameterPoints to produce a single parameter space.
+   * @param firstSpace the first of the two ParameterPoints to join.
+   * @param secondSpace the second of the two ParameterPoints to join.
+   * @return a ParameterPoint which defines the indicated subspace.
+   */
+  public ParameterPoint joinSubspaces(ParameterPoint firstSpace, ParameterPoint secondSpace);
+
+  /**
+   * Split a ParameterPoint into two parameter spaces.
+   * @return an array of two ParameterPoints which define the two subspaces, the first being the head and the second being the tail.
+   */
+  public ParameterPoint [] splitSubspaces(ParameterPoint space);
+
+
+
+} /* ParameterPoint */
