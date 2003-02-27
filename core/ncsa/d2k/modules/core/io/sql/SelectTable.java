972345a74e9e43aaffa812d3b4bf2c7bef37448e
Dora Cai <ycai@illinois.edu>
2003-02-27 17:52:16 +0000

    add validation for no selected tables.

--- core/ncsa/d2k/modules/core/io/sql/SelectTable.java
+++ core/ncsa/d2k/modules/core/io/sql/SelectTable.java
@@ -1,9 +1,9 @@
 package ncsa.d2k.modules.core.io.sql;
 
 import ncsa.d2k.core.modules.*;
-import ncsa.d2k.core.modules.*;
 import ncsa.d2k.userviews.*;
 import ncsa.d2k.userviews.widgets.*;
+import java.awt.event.*;
 
 import java.awt.*;
 import java.util.*;
@@ -17,6 +17,7 @@ import ncsa.d2k.userviews.widgets.swing.*;
 
 */
 public class SelectTable extends ncsa.d2k.core.modules.UIModule {
+  JOptionPane msgBoard = new JOptionPane();
 
 	/**
 		This method returns the description of the various inputs.
@@ -133,23 +134,37 @@ public class SelectTable extends ncsa.d2k.core.modules.UIModule {
 		SelectTablesView
 		This is the UserView class.
 	*/
-	private class SelectTablesView extends ncsa.d2k.userviews.swing.JUserInputPane {
-		JLabel availTablesLabel = new JLabel("Available Tables");
-		DSJList availTablesList = new DSJList();
+	private class SelectTablesView extends JUserPane
+            implements ActionListener {
+            JList availTablesList = new JList ();
+            JButton abort = new JButton ("Abort");
+            JButton done = new JButton ("Done");
+            DefaultListModel availModel = new DefaultListModel();
 
 		/**
 			This method adds the components to a Panel and then adds the Panel
 			to the view.
 		*/
 		public void initView(ViewModule mod) {
-			super.initView(mod);
-			availTablesList.setKey("availTablesList");
-			JPanel p = new JPanel();
-			p.setLayout(new BorderLayout());
-			JScrollPane jsp = new JScrollPane(availTablesList);
-			jsp.setColumnHeaderView(availTablesLabel);
-			p.add(jsp, BorderLayout.CENTER);
-			add(p, BorderLayout.CENTER);
+                  JPanel canvasArea = new JPanel();
+                  canvasArea.setLayout (new BorderLayout ());
+                  JPanel buttons = new JPanel ();
+                  buttons.setLayout (new GridLayout(1,2));
+                  buttons.add (abort);
+                  abort.addActionListener(this);
+                  buttons.add (done);
+                  done.addActionListener(this);
+
+                  JPanel b1 = new JPanel();
+                  b1.add(buttons);
+
+                  availTablesList.setModel(availModel);
+                  JScrollPane jsp = new JScrollPane(availTablesList);
+                  jsp.setColumnHeaderView(new JLabel("Available Tables"));
+
+                  canvasArea.add (jsp, BorderLayout.NORTH);
+                  canvasArea.add (b1, BorderLayout.SOUTH);
+                  add(canvasArea);
 		}
 
 		/**
@@ -162,10 +177,36 @@ public class SelectTable extends ncsa.d2k.core.modules.UIModule {
 		*/
 		public void setInput(Object o, int index) {
 			Vector inputs = (Vector) o;
-			DefaultListModel dlm = new DefaultListModel();
-			for (int i = 0 ; i < inputs.size (); i++)
-				dlm.addElement ((String) inputs.elementAt (i));
-			availTablesList.setModel(dlm);
+                        availModel.removeAllElements();
+                        String longest = null;
+                        int lengthOfLongest = 0;
+                        for (int i = 0 ; i < inputs.size (); i++) {
+                                String elem = (String)inputs.elementAt(i);
+                                availModel.addElement (elem);
+                                if(elem.length() > lengthOfLongest) {
+                                        longest = elem;
+                                        lengthOfLongest = elem.length();
+                                }
+                        }
+                        availTablesList.setPrototypeCellValue(longest);
 		}
+
+                public void actionPerformed(ActionEvent e) {
+                  Object src = e.getSource();
+                  if (src == done) {
+                     if (availTablesList.getSelectedIndex()<0) {
+                       JOptionPane.showMessageDialog(msgBoard,
+                          "You must select a table. ", "Error", JOptionPane.ERROR_MESSAGE);
+                       System.out.println("No table is selected. ");
+                     }
+                     else {
+                       pushOutput(availTablesList.getSelectedValue().toString(), 0);
+                       viewDone("Done");
+                     }
+                  }
+                  else if (src == abort) {
+                    viewCancel();
+                  }
+              }
 	}
 }
