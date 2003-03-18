7b0fa31cef073b0aa368a654d4aa8514b34746ef
Anca Suvaiala <anca@illinois.edu>
2003-03-18 17:03:23 +0000

    changed minimum for number of bins from 1 to 2

--- core/ncsa/d2k/modules/core/transform/binning/AutoBinParamSpaceGenerator.java
+++ core/ncsa/d2k/modules/core/transform/binning/AutoBinParamSpaceGenerator.java
@@ -22,7 +22,7 @@ public class AutoBinParamSpaceGenerator extends AbstractParamSpaceGenerator {
 
     ParameterSpace psi = new ParameterSpaceImpl();
     String[] names = { BIN_METHOD, ITEMS_PER_BIN, NUMBER_OF_BINS};
-    double[] min = { 0, 1, 1};
+    double[] min = { 0, 1, 2};
     double[] max = { 1, Integer.MAX_VALUE, Integer.MAX_VALUE};
     double[] def = { 0, 3, 3};
     int[] res = { 1, 1, 1};
@@ -45,21 +45,26 @@ public class AutoBinParamSpaceGenerator extends AbstractParamSpaceGenerator {
    */
   public PropertyDescription[] getPropertiesDescriptions() {
       PropertyDescription[] pds = new PropertyDescription[3];
-      pds[0] = new PropertyDescription(BIN_METHOD, "Discretization Method",
-				       "The method to use for discretization.  Select 1 to create bins" +
-				       " by weight.  This will create bins with an equal number of items in "+
+      pds[0] = new PropertyDescription(BIN_METHOD, "Discretization Method - Space definition",
+				       "The method to use for discretization.  Only 0 or 1 are valid values. " +
+				       "Select 1 to create bins " +
+				       "by weight.  This will create bins with an equal number of items in "+
 				       "each slot.  Select 0 to discretize by specifying the number of bins. "+
 				       "This will give equally spaced bins between the minimum and maximum for "+
 				       "each scalar column.");
-      pds[1] = new PropertyDescription(ITEMS_PER_BIN, "Number of Items per Bin",
+      pds[1] = new PropertyDescription(ITEMS_PER_BIN, "Number of Items per Bin - Space definition ",
 				       "When binning by weight, this is the number of items" +
-				       " that will go in each bin.  The last bin may have less than "+
-				       "<i>weight</i> values, however.");
-      pds[2] = new PropertyDescription(NUMBER_OF_BINS, "Number of Bins",
-				       "Define the number of bins absolutely. "+
-				       "This will give equally spaced bins between "+
-				       "the minimum and maximum for each scalar "+
-				       "column.");
+				       " that will go in each bin.   However, the bins may contain more or fewer values than "+
+				       "<i>weight</i> values, depending on how many items equal the bin limits. Typically " +
+				       "the last bin will contain less or equal to <i>weight</i> values and the rest of the " +
+				       "bins will contain a number that is  equal or greater to <i>weight</i> values." +
+				       "Minimum value must be 1, maximum is unlimited here but practically is limited by " +
+				       "the number of values.");
+      pds[2] = new PropertyDescription(NUMBER_OF_BINS, "Number of Bins - Space Definition",
+				       "Define the number of bins absolutely. This will give equally spaced bins between "+
+				       "the minimum and maximum for each scalar column. The minimum number of bins can be 2 " +
+				       "and the maximum is unlimited. However, a very high number of bins is likely to results " +
+				       "in a high number of bins with no values.");
     return pds;
   }
 }
