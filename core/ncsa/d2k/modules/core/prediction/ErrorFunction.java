aaf900b48ca7055797f55b5ec310386700fe7f43
David Tcheng <dtcheng@illinois.edu>
2003-03-19 23:34:07 +0000

    simplified error function (elimnated negative output sum)

--- core/ncsa/d2k/modules/core/prediction/ErrorFunction.java
+++ core/ncsa/d2k/modules/core/prediction/ErrorFunction.java
@@ -1,41 +1,44 @@
 package ncsa.d2k.modules.core.prediction;
+
 import ncsa.d2k.modules.core.datatype.parameter.*;
 import java.io.*;
 import ncsa.d2k.modules.core.datatype.table.*;
 import ncsa.d2k.modules.core.datatype.model.*;
 
-public class ErrorFunction implements java.io.Serializable {
+public class ErrorFunction
+    implements java.io.Serializable {
 
-  public int    errorFunctionIndex;
+  public int errorFunctionIndex;
   public String errorFunctionObjectPathName;
 
-  public static final int classificationErrorFunctionIndex     = 0;
-  public static final int absoluteErrorFunctionIndex           = 1;
-  public static final int varianceErrorFunctionIndex           = 2;
-  public static final int likelihoodErrorFunctionIndex         = 3;
+  public static final int classificationErrorFunctionIndex = 0;
+  public static final int absoluteErrorFunctionIndex       = 1;
+  public static final int varianceErrorFunctionIndex       = 2;
+  public static final int likelihoodErrorFunctionIndex     = 3;
 
-  public static final String [] errorFunctionNames = {
-    "classification",
-    "absolute",
-    "variance",
-    "likelihood",
-    "negativeOutputSum",
+  public static final String[] errorFunctionNames = {
+      "classification",
+      "absolute",
+      "variance",
+      "likelihood",
   };
 
   public ErrorFunction(int errorFunctionIndex) {
-    this.errorFunctionIndex          = errorFunctionIndex;
+    this.errorFunctionIndex = errorFunctionIndex;
   }
 
   public ErrorFunction(ParameterPoint parameterPoint) {
-    this.errorFunctionIndex = (int)parameterPoint.getValue(0);
+    this.errorFunctionIndex = (int) parameterPoint.getValue(0);
   }
 
-  static public String [] getErrorFunctionNames() {
+  static public String[] getErrorFunctionNames() {
     return errorFunctionNames;
   }
+
   static public String getErrorFunctionName(int i) throws Exception {
     return errorFunctionNames[i];
   }
+
   static public int getErrorFunctionIndex(String name) throws Exception {
     int index = -1;
     for (int i = 0; i < errorFunctionNames.length; i++) {
@@ -46,8 +49,9 @@ public class ErrorFunction implements java.io.Serializable {
     }
     return index;
   }
-  double [] outputsMemory = new double [0];
-  double [] AllstateRatios;
+
+  double[] outputsMemory = new double[0];
+  double[] AllstateRatios;
 
   public double evaluate(ExampleTable examples, Model model) throws Exception {
     int numExamples = examples.getNumExamples();
@@ -61,41 +65,42 @@ public class ErrorFunction implements java.io.Serializable {
     return errorSum / numExamples;
   }
 
-  public double evaluate(ExampleTable examples, int exampleIndex, Model model) throws Exception {
+  public double evaluate(ExampleTable examples, int exampleIndex, Model model) throws
+      Exception {
 
-    int numInputs  = examples.getNumInputFeatures();
+    int numInputs = examples.getNumInputFeatures();
     int numOutputs = examples.getNumOutputFeatures();
 
     double error = Double.NaN;
 
     // allocate temporary memory if necessary
     if (outputsMemory.length != numOutputs)
-      outputsMemory = new double [numOutputs];
+      outputsMemory = new double[numOutputs];
 
     switch (errorFunctionIndex) {
 
       case classificationErrorFunctionIndex: {
         double errorSum = 0.0;
 
-        double [] outputs = outputsMemory;
+        double[] outputs = outputsMemory;
 
         model.evaluate(examples, exampleIndex, outputs);
 
         if (numOutputs == 1) {
           double predictedOutput = examples.getOutputDouble(exampleIndex, 0);
-          double actualOutput    = outputs[0];
+          double actualOutput = outputs[0];
           if (Math.round(predictedOutput) != Math.round(actualOutput)) {
             errorSum++;
           }
         }
         else {
           double maxPredictedOutput = Double.NEGATIVE_INFINITY;
-          double maxActualOutput    = Double.NEGATIVE_INFINITY;
+          double maxActualOutput = Double.NEGATIVE_INFINITY;
           int maxPredictedOutputIndex = -1;
-          int maxActualOutputIndex    = -1;
+          int maxActualOutputIndex = -1;
           for (int f = 0; f < numOutputs; f++) {
             double predictedOutput = examples.getOutputDouble(exampleIndex, f);
-            double actualOutput    = outputs[f];
+            double actualOutput = outputs[f];
 
             if (predictedOutput > maxPredictedOutput) {
               maxPredictedOutput = predictedOutput;
@@ -117,64 +122,68 @@ public class ErrorFunction implements java.io.Serializable {
       break;
 
       // Absolute Error //
-    case absoluteErrorFunctionIndex: {
-      double errorSum = 0.0;
+      case absoluteErrorFunctionIndex: {
+        double errorSum = 0.0;
 
-      double [] outputs = outputsMemory;
-      model.evaluate(examples, exampleIndex, outputs);
-      for (int f = 0; f < numOutputs; f++) {
-        double difference = outputs[f] - examples.getOutputDouble(exampleIndex, f);
-        errorSum += Math.abs(difference);
+        double[] outputs = outputsMemory;
+        model.evaluate(examples, exampleIndex, outputs);
+        for (int f = 0; f < numOutputs; f++) {
+          double difference = outputs[f] -
+              examples.getOutputDouble(exampleIndex, f);
+          errorSum += Math.abs(difference);
+        }
+
+        error = errorSum;
       }
+      break;
 
-      error = errorSum;
-    }
-    break;
+      // Variance Error //
+      case varianceErrorFunctionIndex: {
+        double errorSum = 0.0;
 
-    // Variance Error //
-  case varianceErrorFunctionIndex: {
-    double errorSum = 0.0;
+        double[] outputs = outputsMemory;
+        model.evaluate(examples, exampleIndex, outputs);
+        for (int f = 0; f < numOutputs; f++) {
+          double difference = outputs[f] -
+              examples.getOutputDouble(exampleIndex, f);
+          errorSum += difference * difference;
+        }
 
-    double [] outputs = outputsMemory;
-    model.evaluate(examples, exampleIndex, outputs);
-    for (int f = 0; f < numOutputs; f++) {
-      double difference = outputs[f] - examples.getOutputDouble(exampleIndex, f);
-      errorSum += difference * difference;
-    }
+        error = errorSum;
+      }
+      break;
 
-    error = errorSum;
-  }
-  break;
+      case likelihoodErrorFunctionIndex: {
 
-case likelihoodErrorFunctionIndex: {
+        double likelihoodSum = 0.0;
 
-  double likelihoodSum = 0.0;
+        double[] predictedOutputs = outputsMemory;
 
-  double [] predictedOutputs = outputsMemory;
+        model.evaluate(examples, exampleIndex, predictedOutputs);
 
-  model.evaluate(examples, exampleIndex, predictedOutputs);
+        for (int f = 0; f < numOutputs; f++) {
 
-  for (int f = 0; f < numOutputs; f++) {
+          double actualOutputClassProbability = examples.getOutputDouble(
+              exampleIndex, f);
 
-    double actualOutputClassProbability = examples.getOutputDouble(exampleIndex, f);
+          double predictedClassProbability = predictedOutputs[f];
 
-    double predictedClassProbability = predictedOutputs[f];
+          if (actualOutputClassProbability == 0.0)
+            predictedClassProbability = 1.0 - predictedClassProbability;
 
-    if (actualOutputClassProbability == 0.0)
-      predictedClassProbability = 1.0 - predictedClassProbability;
+          likelihoodSum += Math.log(predictedClassProbability);
+        }
 
-    likelihoodSum += Math.log(predictedClassProbability);
-  }
+        error = -likelihoodSum;
+      }
+      break;
 
-  error = - likelihoodSum;
-}
-break;
-
-default: {
-  System.out.println("errorFunctionIndex (" + errorFunctionIndex + ") not recognized");
-  error = Double.NaN;
-}
-break;
+      default: {
+        System.out.println("errorFunctionIndex (" + errorFunctionIndex +
+                           ") not recognized");
+        error = Double.NaN;
+      }
+      break;
     }
     return error;
   }
