4987f57c4ab64fddbbe1b7ecc0bbc6693ca0b145
Ruth Aydt <aydt@illinois.edu>
2003-03-24 21:59:12 +0000

    fixed some misspelled labels/words; updated docs some; changed MutableTable input to Table;  no longer have Transformation on output port (no need)

--- core/ncsa/d2k/modules/core/transform/ApplyTransformation.java
+++ core/ncsa/d2k/modules/core/transform/ApplyTransformation.java
@@ -5,64 +5,61 @@ import ncsa.d2k.core.modules.*;
 import ncsa.d2k.modules.core.datatype.table.*;
 
 /**
- * Apply a Transformation to a MutableTable.
+ * Apply a Transformation to a Table.
  */
 public class ApplyTransformation extends DataPrepModule {
 
 	public String[] getInputTypes() {
 		String[] in = {"ncsa.d2k.modules.core.datatype.table.Transformation",
-			"ncsa.d2k.modules.core.datatype.table.MutableTable"};
+			"ncsa.d2k.modules.core.datatype.table.Table"};
 		return in;
-	}
-
-	public String[] getOutputTypes() {
-		String[] out = {"ncsa.d2k.modules.core.datatype.table.Transformation",
-			"ncsa.d2k.modules.core.datatype.table.MutableTable"};
-		return out;
 	}
 
 	public String getInputInfo(int i) {
           switch(i){
-            case 0: return "Trasnformation to apply on  the input Table.";
-            case 1: return "MutableTable to apply the Transformation on.";
+            case 0: return "Transformation to apply to the input Table.";
+            case 1: return "The Table to apply the Transformation to.";
             default: return "no such input";
           }
 	}
 
 	public String getInputName(int i) {
           switch(i){
-            case 0: return "Trasnformation";
+            case 0: return "Transformation";
             case 1: return "Table";
             default: return "no such input";
           }
 	}
 
+	public String[] getOutputTypes() {
+		String[] out = { "ncsa.d2k.modules.core.datatype.table.Table"};
+		return out;
+	}
+
 	public String getOutputInfo(int i) {
         switch(i){
-            case 0: return "the Trasnformation which was applied to the input Table.";
-            case 1: return "the input Table after the Transformation";
+            case 0: return "The Transformed input Table";
             default: return "no such input";
           }
 	}
 
 	public String getOutputName(int i) {
           switch(i){
-            case 0: return "Trasnformation";
-            case 1: return "Table";
+            case 0: return "Table";
             default: return "no such input";
           }
 	}
 
 
         public String getModuleName() {
-          return "Apply Tranform";
+          return "Apply Transformation";
         }
 	public String getModuleInfo() {
-		return "This module applies a Transformation to a Table." +
-                "<P><B>Detailed Description:</b><br>" +
-                "The module applies a Transformation to a MutableTable and outputs " +
-                "the table transformed and the transformation as well." +
-                "<P><B>Data Handling:</b><br>This modules changes the data in the inputs Table</P>";
+		return "<p>Overview: This module applies a Transformation to a Table. " +
+                "</p><P>Detailed Description: " +
+                "This module applies a Transformation to a Table and outputs " +
+                "the transformed table. " +
+                "</p><P>Data Handling: This modules modifies the input Table</P>";
 	}
 
 	public void doit() throws Exception {
@@ -76,15 +73,17 @@ public class ApplyTransformation extends DataPrepModule {
                 //vered - debug
 //                System.out.println( ( (mt.getTransformations().get(0)) == null ? "no " : "there are " + "transformations" ));
 
-		pushOutput(t, 0);
-		pushOutput(mt, 1);
+		pushOutput(mt, 0);
 	}
 }
 
-/**
- * QA comments
- * 28-2-03 through this module was not yet handed off to the qa, due to the fact
- *         that the other tranformation creators modules used it in itineraries
- *         i took the liberty to add names and info to inputs and outputs.
- *         vered.
- */
+//
+// QA comments
+// 28-2-03 through this module was not yet handed off to the qa, due to the fact
+//         that the other tranformation creators modules used it in itineraries
+//         i took the liberty to add names and info to inputs and outputs.
+//         vered.
+// 24-3-03 still not handed off to QA but... updates to descriptions; now takes table
+//         intstead of mutable table;  no longer has table on output.  needs
+//         developer to verify that use of table/mutable table is correct. should
+//         it be just table throughout?  ruth
