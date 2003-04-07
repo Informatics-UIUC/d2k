15f59e38c23b20855c06c42815ebb50725b91729
Peter Groves <pgroves@illinois.edu>
2003-04-07 06:15:51 +0000

    *** empty log message ***

--- core/ncsa/d2k/modules/core/control/MultiPusher.java
+++ core/ncsa/d2k/modules/core/control/MultiPusher.java
@@ -1,9 +1,6 @@
-
 package ncsa.d2k.modules.core.control;
 
-
 import ncsa.d2k.core.modules.*;
-import java.io.Serializable;
 /**
 	MultiPusher.java
 
@@ -12,95 +9,54 @@ import java.io.Serializable;
 
 	@author Peter Groves
 	7/15/01
+	
+	Revised 04/06/03 by pgroves
 */
-public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule 
+public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
+	implements java.io.Serializable 
 {
 
-	protected boolean debug=false;
 
-
-	/**
-		This pair returns the description of the various inputs.
-		@return the description of the indexed input.
-	*/
-	public String getInputInfo(int index) {
-		switch (index) {
-			case 0: return "      The object to be passed multiple times   ";
-			case 1: return "      The number of times to pass it  ";
-			default: return "No such input";
-		}
-	}
-
-	/**
-		This pair returns an array of strings that contains the data types for the inputs.
-		@return the data types of all inputs.
-	*/
-	public String[] getInputTypes() {
-		String[] types = {"java.lang.Object","java.lang.Integer"};
-		return types;
-	}
-
-	/**
-		This pair returns the description of the outputs.
-		@return the description of the indexed output.
-	*/
-	public String getOutputInfo(int index) {
-		switch (index) {
-			case 0: return "      One of multiple pushes of the object   ";
-			default: return "No such output";
-		}
-	}
-
-	/**
-		This pair returns an array of strings that contains the data types for the outputs.
-		@return the data types of all outputs.
-	*/
-	public String[] getOutputTypes() {
-		String[] types = {"java.lang.Object"};
-		return types;
-	}
-
-	/**
-		This pair returns the description of the module.
-		@return the description of the module.
-	*/
-	public String getModuleInfo() {
-		return "<html>  <head>      </head>  <body>    Takes an Object, pushes it out N times.PROPS: N-the number of pushes,     usePropNValue - false- will wait for and use the Integer in input 1; true     - will use the N in properties  </body></html>";
-	}
-	/* the number of times to push the object*/
+	//////////////////////
+	//d2k Props
+	////////////////////
+	
+	/** the number of times to push the object*/
 	int N=4;
-	public void setN(int i){
-		N=i;
-	}
-	public int getN(){
-		return N;
-	}
 
-	/*false- will wait for and use the Integer in input 1
+	
+	/**false- will wait for and use the Integer in input 1
 		true - will use the N in properties*/
 	private boolean usePropNValue=false;
-	public void setUsePropNValue(boolean b){
-		usePropNValue=b;
-	}
-	public boolean getUsePropNValue(){
-		return usePropNValue;
-	}
-
+	
+	boolean debug=false;		
+	/////////////////////////
+	/// other fields
+	////////////////////////
+	/** the current number of times this module has fired, indicating the
+	number of times the input object has been pushed*/
 	int numFires=0;
+	
+	/** the object that is input (pulled in)*/
+	Object obj;
+	
+	/** the total number of times this module has fired since the itin began,
+		this is used only for debugging*/
+	int totalFires=0;
 
-	public void beginExecution(){
-		numFires=0;
-		totalFires=0;
-		super.beginExecution();
-	}
-
-	/*
-		checks to see if the object is in and  if it should wait for input (1)
-		(the Integer N), otherwise returns the superclass's
-		isReady
+	//////////////////////////
+	///d2k control methods
+	///////////////////////
+	
+	/**isReady()
+	
+		At first, checks to see if the object is in and if it should wait for
+		input (1) (the Integer N), otherwise returns the superclass's isReady.
+		Once it has the object and knows how many times to pass it, returns true
+		until it has been triggered all N times.
 		*/
 	public boolean isReady(){
-		if(	numFires==0){//this is the 'first' run
+		if(numFires==0){//this is the 'first' run
 			if( (inputFlags[0]>0)&&//we have the object to push
 				((usePropNValue)||(inputFlags[1]>0))){//we have the number of times
 				return true;
@@ -108,15 +64,23 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
 				return false;
 			}
 		}
-		if((0<numFires)&&(numFires<N)){
+		if((0<numFires)&&(numFires<N)){//we have everything, pushing N times
 			return true;
 		}
 
 		return false;
 	}
 
-	Object obj;
-	int totalFires=0;
+	public void beginExecution(){
+		numFires=0;
+		totalFires=0;
+		super.beginExecution();
+	}
+	
+	/////////////////////
+	//work methods
+	////////////////////
+
 	/**
 		does it every time
 	*/
@@ -132,22 +96,21 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
 
 		pushOutput(obj, 0);
 		numFires++;
-
 		totalFires++;
+		
 		if(debug)
-		System.out.println("MultiPush: numFires:"+numFires+" total:"+totalFires);
+			System.out.println(this.getAlias()+" current numFires:"+numFires+
+			"/"+N+", total number fires this execution:"+totalFires);
 		if(numFires==N){
 			obj=null;
 			numFires=0;
 		}
 	}
-	public void setDebug(boolean b){
-		debug=b;
-	}
-	public boolean getDebug(){
-		return debug;
-	}
-
+		
+	
+	////////////////////////
+	/// D2K Info Methods
+	/////////////////////
 
 	/**
 	 * Return the human readable name of the module.
@@ -156,6 +119,16 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
 	public String getModuleName() {
 		return "MultiPusher";
 	}
+/**
+		This pair returns the description of the module.
+		@return the description of the module.
+	*/
+	public String getModuleInfo() {
+		return "Takes any object, pushes it out N times, where N is either set"+
+		" as a property or passed in as an input. See property descriptions "+
+		" for details on controlling this behaviour";
+	}
+
 
 	/**
 	 * Return the human readable name of the indexed input.
@@ -165,13 +138,40 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
 	public String getInputName(int index) {
 		switch(index) {
 			case 0:
-				return "objIn";
+				return "Object In";
 			case 1:
 				return "N";
 			default: return "NO SUCH INPUT!";
 		}
 	}
 
+	/**
+		This pair returns the description of the various inputs.
+		@return the description of the indexed input.
+	*/
+	public String getInputInfo(int index) {
+		switch (index) {
+			case 0: 
+				return "The object to be passed multiple times";
+			case 1: 
+				return "The number of times to pass the input object. This module"+
+					" must receive exactly one Integer object for every object"+
+					" passed to the first input (unless the propery <i> Use the "+
+					"value of \"N\" from the properties</i> is set to TRUE)";
+				
+			default: return "No such input";
+		}
+	}
+
+	/**
+		This pair returns an array of strings that contains the data types
+		for the inputs.
+		@return the data types of all inputs.
+	*/
+	public String[] getInputTypes() {
+		String[] types = {"java.lang.Object","java.lang.Integer"};
+		return types;
+	}
 	/**
 	 * Return the human readable name of the indexed output.
 	 * @param index the index of the output.
@@ -180,9 +180,82 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
 	public String getOutputName(int index) {
 		switch(index) {
 			case 0:
-				return "objOut";
+				return "Object Out";
 			default: return "NO SUCH OUTPUT!";
 		}
 	}
+
+	/**
+		This pair returns the description of the outputs.
+		@return the description of the indexed output.
+	*/
+	public String getOutputInfo(int index) {
+		switch (index) {
+			case 0: return "One of multiple pushes of the object.";
+			default: return "No such output";
+		}
+	}
+
+	/**
+		This pair returns an array of strings that contains the data types 
+		for the outputs.
+		@return the data types of all outputs.
+	*/
+	public String[] getOutputTypes() {
+		String[] types = {"java.lang.Object"};
+		return types;
+	}
+
+	  /**
+     * Return a list of the property descriptions.
+     * @return a list of the property descriptions.
+     */
+    public PropertyDescription[] getPropertiesDescriptions() {
+      PropertyDescription[] pds = new PropertyDescription[3];
+      pds[0] = new PropertyDescription("usePropNValue",
+		 "Use the value of \"N\" from the properties",
+        "If TRUE, the number of times the object is pushed will be determined"+
+		  " by the <i>N - Number Times to Push</i> property. In this case, the "+
+		  "module will not wait for the second input before firing. If FALSE,"+
+		  " the value of \"N\" will be determined by the second input.");
+      pds[2] = new PropertyDescription("debug",
+        "Debug",
+        "If true, will print the number of times the module has fired, every "+
+		  "time it is fired.");
+		pds[1] = new PropertyDescription("timesToFire", 
+			"N - Number Times to Push",
+       	"The number of times to pass the input object");
+      return pds;
+    }
+
+
+	////////////////////////////////
+	//D2K Property get/set methods
+	///////////////////////////////
+	public void setUsePropNValue(boolean b){
+		usePropNValue=b;
+	}
+	public boolean getUsePropNValue(){
+		return usePropNValue;
+	}
+	public void setTimesToFire(int i){
+		N=i;
+	}
+	public int getTimesToFire(){
+		return N;
+	}
+	public void setDebug(boolean b){
+		debug=b;
+	}
+	public boolean getDebug(){
+		return debug;
+	}
 }
+			
+					
+
+			
+
+								
+	
 
