9c1492f34daae445eff356b89db76e3d19308ba8
Loretta Auvil <lauvil@illinois.edu>
2003-04-10 14:48:07 +0000

    moved this file from transform/table to transform/attribute.. -Loretta

---
+++ core/ncsa/d2k/modules/core/transform/attribute/ReplaceNominalsWithInts.java
@@ -0,0 +1,113 @@
+package ncsa.d2k.modules.core.transform.attribute;
+
+import java.util.*;
+import ncsa.d2k.core.modules.*;
+import ncsa.d2k.modules.core.datatype.table.*;
+import ncsa.d2k.modules.core.datatype.table.transformations.*;
+
+/**
+ * This module outputs a (reversible) transformation on a
+ * <code>MutableTable</code> that replaces unique nominal column values with
+ * unique integers.
+ */
+public class ReplaceNominalsWithInts extends ComputeModule {
+
+   private boolean _printMapping = false;
+   public boolean getPrintMapping() { return _printMapping; }
+   public void setPrintMapping(boolean value) { _printMapping = value; }
+
+   public void doit() {
+
+      MutableTable mt = (MutableTable)pullInput(0);
+
+      ReplaceNominalValuesWithIntegersTransform transform =
+            new ReplaceNominalValuesWithIntegersTransform(mt);
+
+      if (_printMapping)
+         System.out.println(transform.toMappingString(mt));
+
+      pushOutput(transform, 0);
+
+   }
+
+   public String getInputInfo(int index) {
+      if (index == 0)
+         return "The <i>MutableTable</i> to be used in constructing the " +
+                "transformation.";
+      return null;
+   }
+
+   public String getInputName(int index) {
+      if (index == 0)
+         return "Mutable Table";
+      return null;
+   }
+
+   public String[] getInputTypes() {
+      return new String[] {
+         "ncsa.d2k.modules.core.datatype.table.MutableTable"
+      };
+   }
+
+   public String getModuleInfo() {
+      StringBuffer sb = new StringBuffer("<p>Overview: ");
+      sb.append("This module constructs a reversible transformation to ");
+      sb.append("replace each unique value in every nominal column of a ");
+      sb.append("<i>MutableTable</i> with an integer unique to that column.");
+
+      sb.append("</P><P><u>Missing Values Handling:</u> This module handles missing values as if they were " +
+         "real meaningful values. For Example: If a missing value in a string column " +
+         "is represented by '?', then after applying the transformation, a unique integer will "+
+         "be assigned to all the missing values");
+
+      sb.append("</p><p>Data Handling: ");
+      sb.append("This module does not modify its input data. Rather, its ");
+      sb.append("output is a <i>ReversibleTransformation</i> that can be ");
+      sb.append("applied downstream.");
+      sb.append("</p>");
+      return sb.toString();
+   }
+
+   public String getModuleName() {
+      return "Replace Nominals With Ints";
+   }
+
+   public String getOutputInfo(int index) {
+      if (index == 0)
+         return "The <i>ReversibleTransformation</i> that performs the " +
+                "replacement of column values.";
+      return null;
+   }
+
+   public String getOutputName(int index) {
+      if (index == 0)
+         return "Reversible Transformation";
+      return null;
+   }
+
+   public String[] getOutputTypes() {
+      return new String[] {
+         "ncsa.d2k.modules.core.datatype.table.ReversibleTransformation"
+      };
+   }
+
+   public PropertyDescription[] getPropertiesDescriptions() {
+
+      PropertyDescription[] pds = new PropertyDescription[1];
+
+      pds[0] = new PropertyDescription("printMapping",
+         "Print Integer Mappings",
+         "If enabled, the nominal-to-integer mappings for each column will " +
+         "be printed to the console.");
+
+      return pds;
+
+   }
+
+}
+
+/**
+ * QA comments:
+ * 2-28-03  Vered started qa.
+ *          added to module info a note about missing values handling.
+ */
