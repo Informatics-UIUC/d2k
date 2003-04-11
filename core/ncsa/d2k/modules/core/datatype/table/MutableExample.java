fe6e684e7a06b36e249a5debb2090e20a4fe7639
Unknown <unknown@illinois.edu>
2003-04-11 03:20:55 +0000

    This commit was manufactured by cvs2svn to create branch 'core2basic'.

--- core/ncsa/d2k/modules/core/datatype/table/MutableExample.java
+++
@@ -1,7 +0,0 @@
-package ncsa.d2k.modules.core.datatype.table;
-
-public interface MutableExample extends Example {
-    public double setInputDouble(int i, double value);
-    public double setOutputDouble(int i, double value);
-    public MutableExample copy();
-}
