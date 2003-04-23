9cbb629559bc608628e24d9c13ffb7f082a627c6
Bradley Berkin <berkin@illinois.edu>
2003-04-23 02:06:04 +0000

    Revision 1.1

--- basic/src/ncsa/d2k/modules/core/vis/widgets/ClusterBarChart.java
+++ basic/src/ncsa/d2k/modules/core/vis/widgets/ClusterBarChart.java
@@ -3,383 +3,124 @@ package ncsa.d2k.modules.core.vis.widgets;
 import java.awt.*;
 import java.awt.geom.*;
 import java.text.*;
-import java.util.*;
-import javax.swing.*;
-
 import ncsa.d2k.modules.core.datatype.table.*;
-import ncsa.d2k.modules.core.datatype.table.basic.*;
-import ncsa.d2k.modules.core.datatype.table.util.*;
 
 public class ClusterBarChart extends BarChart {
-  private static final int LEFTOFFSET = 20;
-  private static final int RIGHTOFFSET = 20;
-  private static final int TOPOFFSET = 20;
-  private static final int BOTTOMOFFSET = 20;
 
-  // Clustering
-  private int runs = 0;
-  private int runsize;
+   private int distinctTimeStamps;
 
-  // Unique cluster values
-  private HashSet valueset;
+   public ClusterBarChart(Table table, DataSet set, GraphSettings settings) {
 
-  // Data
-  private MutableTable mutable;
+      super(table, set, settings);
 
-  private int longest_font_width_z;
+      settings.displayaxislabels = false;
 
-  public ClusterBarChart(Table table, DataSet set, GraphSettings settings, int xincrement, int yincrement) throws Exception {
-    super(table, set, settings);
-
-    settings.displayaxislabels = false;
-    gridsize = settings.gridsize;
-
-    minimumxoffsetincrement = xincrement;
-    minimumyoffsetincrement = yincrement;
-
-    yvalueincrement = (ymaximum-yminimum)/gridsize;
-
-    // Dependencies
-    if (table.getNumRows() == 0)
-      throw new Exception("Table exception");
-
-    if (!table.isColumnNominal(set.z))
-      throw new Exception("Nominal exception");
-
-    mutable = table.toExampleTable();
-
-    // Map x, y and z to first three columns
-    int[] reorder = {set.x, set.y, set.z};
-    mutable = (MutableTable) mutable.reorderColumns(reorder);
-    if (mutable.getNumColumns() > 3)
-      mutable.removeColumns(3, mutable.getNumColumns()-3);
-
-    // Runs
-    int[] sort = {0, 2};
-    int[] indices = TableUtilities.multiSortIndex(mutable, sort);
-    mutable = (MutableTable) mutable.reorderRows(indices);
-
-    // Missing values
-    valueset = TableUtilities.uniqueValueSet(mutable, 2);
-    runsize = valueset.size();
-
-    // Determine number of runs
-    String label = mutable.getString(0, 0);
-    HashSet runset = new HashSet(valueset);
-    int rows = mutable.getNumRows();
-    for (int row=0; row < rows; row++) {
-      String runlabel = mutable.getString(row, 0);
-      String runtime = mutable.getString(row, 2);
-
-      if (!runlabel.equals(label)) {
-        // Missing values
-        Iterator iterator = runset.iterator();
-        while (iterator.hasNext()) {
-          String[] values = new String[mutable.getNumColumns()];
-          values[0] = label;
-          values[1] = new String("0.0");
-          values[2] = (String) iterator.next();
-          mutable.addRow(values);
-        }
-
-        runs++;
-        label = runlabel;
-
-        runset = new HashSet(valueset);
-      }
-      runset.remove(runtime);
-    }
-
-    // Last run
-    Iterator iterator = runset.iterator();
-    while (iterator.hasNext()) {
-      String[] values = new String[mutable.getNumColumns()];
-      values[0] = label;
-      values[1] = new String("0.0");
-      values[2] = (String) iterator.next();
-      mutable.addRow(values);
-    }
-    runs++;
-
-    indices = TableUtilities.multiSortIndex(mutable, sort);
-    mutable = (mutable.reorderRows(indices)).toExampleTable();
-
-    // Include bins for spacing runs
-    // Impacts mapping of bins to table values
-    bins = (runsize+1)*runs;
-  }
-
-  public void initOffsets() {
-    NumberFormat numberformat = NumberFormat.getInstance();
-    numberformat.setMaximumFractionDigits(3);
-
-    // Determine maximum string widths
-    // X axis
-    for (int run=0; run < runs; run++) {
-      String value = mutable.getString(run*runsize, 0);
-
-      int stringwidth = metrics.stringWidth(value);
-      if (stringwidth > longest_font_width_x)
-        longest_font_width_x = stringwidth;
-    }
-
-    // Y axis
-    double yvalue =  yminimum;
-    for (int index=0; index < gridsize; index++) {
-      String value = numberformat.format(yvalue);
-
-      int stringwidth = metrics.stringWidth(value);
-      if (stringwidth > longest_font_width_y)
-        longest_font_width_y = stringwidth;
-
-      yvalue += yvalueincrement;
-    }
-
-    // Z axis
-    String label = mutable.getColumnLabel(2);
-    int labelwidth = metrics.stringWidth(label);
-    if (labelwidth > longest_font_width_x)
-      longest_font_width_z = labelwidth;
-
-    // Determine offsets
-    if (!settings.displaylegend) {
-      legendheight = 0;
-      legendwidth = 0;
-    }
-    else {
-      String[] values = TableUtilities.uniqueValues(mutable, 2);
-      legendwidth = metrics.stringWidth(values[0]);
-      for (int index=1; index < values.length; index++) {
-        int stringwidth = metrics.stringWidth(values[index]);
-        if (stringwidth > legendwidth)
-          legendwidth = stringwidth;
+      double currentTime = Double.NEGATIVE_INFINITY;
+      for (int row = 0; row < table.getNumRows(); row++) {
+         if (table.getDouble(row, set.z) < currentTime) {
+            break;
+         }
+         else {
+            currentTime = table.getDouble(row, set.z);
+            distinctTimeStamps++;
+         }
       }
 
-      if (legendwidth > longest_font_width_z)
-        legendwidth += 3*smallspace+samplecolorsize;
-      else
-        legendwidth = longest_font_width_z;
-
-      legendheight = (fontheight+smallspace)+(values.length*fontheight);
-    }
-
-    // Primary offsets
-    leftoffset = LEFTOFFSET+point_size+longest_font_width_y;
-    rightoffset = RIGHTOFFSET+legendwidth+2*legendspace;
-    bottomoffset = BOTTOMOFFSET+point_size+longest_font_width_x;
-    topoffset = TOPOFFSET+point_size;
-
-    // Minimum dimensions
-    minimumgraphwidth = minimumxoffsetincrement*bins+leftoffset+rightoffset;
-    minimumgraphheight = Math.max(minimumyoffsetincrement*gridsize+topoffset+bottomoffset,
-                                  legendheight+topoffset+bottomoffset);
-
-    // Legend offsets
-    legendleftoffset = getGraphWidth()-legendwidth-legendspace;
-    legendtopoffset = getGraphHeight()/2-legendheight/2;
-  }
-
-  // Resize scale
-  public void resize() {
-    xoffsetincrement = (getGraphWidth()-leftoffset-rightoffset)/bins;
-    yoffsetincrement = (getGraphHeight()-topoffset-bottomoffset)/gridsize;
-    yscale = (ymaximum-yminimum)/(graphheight-topoffset-bottomoffset);
-
-    /*
-    while ((yoffsetincrement < minimumyoffsetincrement) && (gridsize > 0)) {
-      gridsize = gridsize/2;
-      yoffsetincrement = (graphheight-topoffset-bottomoffset)/gridsize;
-    }
-    */
-  }
-
-  public int getGraphWidth() {
-    if (getWidth() < minimumgraphwidth)
-      return (int) minimumgraphwidth;
-
-    return getWidth();
-  }
-
-  public int getGraphHeight() {
-    if (getHeight() < minimumgraphheight)
-      return (int) minimumgraphheight;
-
-    return getHeight();
-  }
-
-  public Dimension getPreferredSize() {
-    return getMinimumSize();
-  }
-
-  public Dimension getMinimumSize() {
-    return new Dimension((int) minimumgraphwidth, (int) minimumgraphheight);
-  }
-
-  /*
-  Drawing functions
-  */
-
-  public void drawTitle(Graphics2D g2) {
-    int stringwidth = metrics.stringWidth(title);
-    double x = (getGraphWidth()-stringwidth)/2;
-    double y = (topoffset)/2 + fontheight/2;
-
-    g2.drawString(title, (int) x, (int) y);
-  }
-
-  public void drawAxis(Graphics2D g2) {
-    g2.draw(new Line2D.Double(leftoffset, topoffset,
-                              leftoffset, getGraphHeight()-bottomoffset));
-    g2.draw(new Line2D.Double(leftoffset, getGraphHeight()-bottomoffset,
-                              getGraphWidth()-rightoffset, getGraphHeight()-bottomoffset));
-  }
-
-  public void drawTickMarks(Graphics2D g2) {
-    double x = leftoffset+xoffsetincrement/2;
-
-    // Map bins to runs
-    int counter = 0;
-    int offset = 0;
-
-    for (int bin=0; bin < bins; bin++) {
-
-      if (counter == runsize)
-        counter = 0;
-
-      else {
-        g2.draw(new Line2D.Double(x, getGraphHeight()-bottomoffset-tickmarksize, x, getGraphHeight()-bottomoffset+tickmarksize));
-        counter++;
+      bins = 0;
+      for (int row = 0; row < table.getNumRows(); row++) {
+         bins++;
+         if (row > 0 && (row + 1) % distinctTimeStamps == 0 && row != table.getNumRows() - 1)
+            bins++;
       }
 
-      x += xoffsetincrement;
-    }
+   }
 
-    double y = topoffset+yoffsetincrement;
-    for (int bin=0; bin < gridsize; bin++) {
-      g2.draw(new Line2D.Double(leftoffset-tickmarksize, y, leftoffset+tickmarksize, y));
-      y += yoffsetincrement;
-    }
-  }
+   public void drawScale(Graphics2D g2) {
+      NumberFormat numberformat = NumberFormat.getInstance();
+      numberformat.setMaximumFractionDigits(3);
 
-  public void drawGrid(Graphics2D g2) {
-    Color previouscolor = g2.getColor();
-    g2.setColor(Color.gray);
+      // x axis
+      // TextualColumn textcolumn;
+      double x = leftoffset+(xoffsetincrement/2);
+      double lastx = 0;
+      AffineTransform transform = g2.getTransform(); //
+      g2.rotate(Math.toRadians(-90));
+      int ascent = metrics.getAscent();
 
-    // y axis
-    double y = topoffset+yoffsetincrement;
-    for (int index=0; index < gridsize-1; index++) {
-      g2.draw(new Line2D.Double(leftoffset, y, getGraphWidth()-rightoffset, y));
-      y += yoffsetincrement;
-    }
+      int binOffset = 0;
+      boolean nextRunToggle = false;
 
-    g2.setColor(previouscolor);
-  }
+      for (int row=0; row < bins; row++) {
 
-  public void drawScale(Graphics2D g2) {
-    NumberFormat numberformat = NumberFormat.getInstance();
-    numberformat.setMaximumFractionDigits(3);
-    int ascent = metrics.getAscent();
+         int index = row - binOffset;
 
-    double xincrement = (runsize+1)*xoffsetincrement;
-
-    double x = leftoffset + (xincrement/2);
-
-    AffineTransform transform = g2.getTransform();
-    g2.rotate(Math.toRadians(90));
-
-    for (int run=0; run < runs; run++) {
-      String value = mutable.getString(run*runsize, 0);
-      int stringwidth = metrics.stringWidth(value);
-
-      g2.drawString(value,
-                    (int) (getGraphHeight()-bottomoffset+tickmarksize+smallspace),
-                    (int) -(x-ascent/2));
-      x += xincrement;
-    }
-
-    g2.setTransform(transform);
-
-    double y = getGraphHeight()-bottomoffset;
-    double yvalue =  yminimum;
-    for (int index=0; index < gridsize; index++) {
-      String value = numberformat.format(yvalue);
-      int stringwidth = metrics.stringWidth(value);
-
-      g2.drawString(value, (int) (leftoffset-stringwidth-smallspace), (int) (y+fontascent/2));
-      y -= yoffsetincrement;
-      yvalue += yvalueincrement;
-    }
-  }
-
-  public void drawLegend(Graphics2D g2) {
-    Color previouscolor = g2.getColor();
-
-    double x = legendleftoffset;
-    double y = legendtopoffset;
-
-    g2.drawString(mutable.getColumnLabel(2), (int) x, (int) y);
-
-    y += smallspace;
-
-    g2.draw(new Rectangle.Double(x, y, legendwidth, legendheight));
-
-    x += smallspace;
-    y += fontheight-samplecolorsize;
-
-    String[] values = new String[runsize];
-    mutable.getSubset(0, runsize).getColumn(values, 2);
-    for (int index=0; index < values.length; index++) {
-      double gradient = (double) ((double) index)/((double) runsize);
-      g2.setColor(new Color((int)(255 - 255*gradient), 25, (int)(255*gradient)));
-      g2.fill(new Rectangle.Double(x, y, samplecolorsize, samplecolorsize));
-      y += fontheight;
-    }
-
-    g2.setColor(previouscolor);
-
-    x = legendleftoffset;
-    y = legendtopoffset;
-
-    x += 2*smallspace+samplecolorsize;
-    y += fontheight+smallspace;
-
-    for (int index=0; index < values.length; index++) {
-      String value = values[index];
-      g2.drawString(value, (int) x, (int) y);
-      y += fontheight;
-    }
-  }
-
-  public void drawDataSet(Graphics2D g2, DataSet set) {
-    double x = leftoffset;
-    double barwidth = xoffsetincrement;
-
-    // Map bins to runs
-    int counter = 0;
-    int offset = 0;
-
-    for (int bin=0; bin < bins; bin++) {
-
-      if (counter == runsize) {
-        counter = 0;
-        offset++;
+         if (!nextRunToggle && row > 0 && (index) % distinctTimeStamps == 0) {
+            binOffset++;
+            nextRunToggle = true;
+         }
+         else if (x - ascent/2 + 3 > lastx) {
+            String bin = (String)table.getString(index, set.x);
+            String time = (String)table.getString(index, set.z);
+            int stringwidth = metrics.stringWidth(bin + ", " + time);
+            g2.drawString(bin + ", " + time,
+               (int)-(graphheight-bottomoffset+stringwidth+tickmarksize+3),
+               (int)(x+ascent/2));
+            lastx = x + ascent/2;
+            nextRunToggle = false;
+         }
+         x += xoffsetincrement;
       }
 
-      else {
-        double value = mutable.getDouble(bin-offset, 1);
-        double barheight = (value-yminimum)/yscale;
-        double y = getGraphHeight()-bottomoffset-barheight;
+      g2.setTransform(transform);
 
-        double gradient = (double)((bin-offset) % runsize)/(double)runsize;
-        g2.setColor(new Color((int)(255 - 255*gradient), 25, (int)(255*gradient)));
-        g2.fill(new Rectangle2D.Double(x, y, barwidth, barheight));
-        g2.setColor(Color.black);
-        g2.draw(new Rectangle2D.Double(x, y, barwidth, barheight));
+      // y axis
+      double yvalue =  yminimum;
+      double y = graphheight-bottomoffset;
+      for (int index=0; index < gridsize; index++) {
+         String string = numberformat.format(yvalue);
+         int stringwidth = metrics.stringWidth(string);
+         g2.drawString(string, (int) (leftoffset-stringwidth-smallspace),
+            (int) (y+fontascent/2));
+         y -= yoffsetincrement;
+         yvalue += yvalueincrement;
+      }
+   }
 
-        counter++;
+   public void drawDataSet(Graphics2D g2, DataSet set) {
+
+      double x = leftoffset;
+      double barwidth = xoffsetincrement;
+
+      int binOffset = 0;
+      boolean nextRunToggle = false;
+
+      for (int row=0; row < bins; row++) {
+
+         int index = row - binOffset;
+
+         double value = table.getDouble(index, set.y);
+         double y = graphheight-bottomoffset-(value-yminimum)/yscale;
+         double barheight = (value-yminimum)/yscale;
+
+         if (!nextRunToggle && row > 0 && (index) % distinctTimeStamps == 0) {
+            binOffset++;
+            x += xoffsetincrement;
+            nextRunToggle = true;
+         }
+         else {
+            double gradient = (double)(index % distinctTimeStamps)/(double)distinctTimeStamps;
+
+            // g2.setColor(new Color(102, 153, 255));
+            g2.setColor(new Color((int)(255 - 255*gradient), 51, (int)(255*gradient)));
+            g2.fill(new Rectangle2D.Double(x, y, barwidth, barheight));
+            g2.setColor(Color.black);
+            g2.draw(new Rectangle2D.Double(x, y, barwidth, barheight));
+            x += xoffsetincrement;
+            nextRunToggle = false;
+         }
+
       }
 
-      x += xoffsetincrement;
-    }
-  }
+   }
+
 }
