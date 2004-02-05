08c191b570e330a301a6ada75c8f3404baf223bc
David Clutter <clutter@illinois.edu>
2004-02-05 17:06:20 +0000

    Added support for constraints to the Solution interface.  three methods were defined: getNumConstraints(), setConstraint() and getConstraint().  BinarySolution, DoubleSolution, IntSolution, and MixedSolution all implement these methods.
     an additional constructor has been added to all classes that implement the Solution interface to specify the number of constraints.
    
    the clone() and copyMe() methods of classes that implement solutions have been modified to copy the constraints
    
    Added the ConstrainedPopulation interface which defines three methods: getNumConstraints(), setConstraintWeight() getConstraintWeight().  ConstrainedNsgaPopulation implements this interface

--- core/ncsa/d2k/modules/core/optimize/ga/emo/NsgaSolution.java
+++ core/ncsa/d2k/modules/core/optimize/ga/emo/NsgaSolution.java
@@ -7,40 +7,30 @@ import ncsa.d2k.modules.core.optimize.util.*;
  */
 
 public interface NsgaSolution extends MOSolution {
-	/**
-	 * returns the constraint value, representing a constraint violation.
-	 * @returns the constraint value, representing degree constraint violation.
-	 */
-	public double getConstraint ();
 
-	/**
-	 * sets the constraint value.
-	 * @param the new constraint value.
-	 */
-	public void setConstraint (double rank);
 
-	/**
-	 * returns the non-dominated front index, or rank.
-	 * @param returns the non-dominated front index, or rank.
-	 */
-	public int getRank ();
+        /**
+         * returns the non-dominated front index, or rank.
+         * @param returns the non-dominated front index, or rank.
+         */
+        public int getRank ();
 
-	/**
-	 * sets the non-dominated front index, or rank.
-	 * @param the non-dominated front index, or rank.
-	 */
-	public void setRank (int rank);
+        /**
+         * sets the non-dominated front index, or rank.
+         * @param the non-dominated front index, or rank.
+         */
+        public void setRank (int rank);
 
-	/**
-	 * returns the crowding distance.
-	 * @returns the crowding distance.
-	 */
-	public double getCrowdingDistance ();
+        /**
+         * returns the crowding distance.
+         * @returns the crowding distance.
+         */
+        public double getCrowdingDistance ();
 
-	/**
-	 * sets the crowding distance
-	 * @param crowding the new crowding distance.
-	 */
-	public void setCrowdingDistance (double crowding);
+        /**
+         * sets the crowding distance
+         * @param crowding the new crowding distance.
+         */
+        public void setCrowdingDistance (double crowding);
 
 }
--- core/ncsa/d2k/modules/core/optimize/ga/emo/examples/GenerateFinancialPopulation.java
+++ core/ncsa/d2k/modules/core/optimize/ga/emo/examples/GenerateFinancialPopulation.java
@@ -7,79 +7,79 @@ import ncsa.d2k.core.modules.*;
 import java.io.Serializable;
 
 /**
-		Generate financial data
+                Generate financial data
 
 */
 public class GenerateFinancialPopulation extends PopulationPrep  {
-	public GenerateFinancialPopulation () {
-	}
+        public GenerateFinancialPopulation () {
+        }
 
-	/**
-		This method returns the description of the module.
+        /**
+                This method returns the description of the module.
 
-		@return the description of the module.
-	*/
-	public String getModuleInfo () {
-		return "<html>  <head>      </head>  <body>    This module sets up the initial population, and will set all the fields of     the population that are used to steer the genetic algorithm.  </body></html>";
-	}
+                @return the description of the module.
+        */
+        public String getModuleInfo () {
+                return "<html>  <head>      </head>  <body>    This module sets up the initial population, and will set all the fields of     the population that are used to steer the genetic algorithm.  </body></html>";
+        }
 
-	/**
-		Create the initial population. In this case we have chosen to override the doit method,
-		though it was probably not necessary
+        /**
+                Create the initial population. In this case we have chosen to override the doit method,
+                though it was probably not necessary
 
-		@param outV the array to contain output object.
-	*/
-	public void doit () throws Exception {
+                @param outV the array to contain output object.
+        */
+        public void doit () throws Exception {
 
-		// First define the binary range, in this case 8 bits.
-		DoubleRange [] xyz = new DoubleRange [5];
-		xyz [0] = new DoubleRange ("x1",0.3, 0.0);
-		xyz [1] = new DoubleRange ("x2",0.3, 0.0);
-		xyz [2] = new DoubleRange ("x3",0.3, 0.0);
-		xyz [3] = new DoubleRange ("x4",0.3, 0.0);
-		xyz [4] = new DoubleRange ("x5",0.3, 0.0);
+                // First define the binary range, in this case 8 bits.
+                DoubleRange [] xyz = new DoubleRange [5];
+                xyz [0] = new DoubleRange ("x1",0.3, 0.0);
+                xyz [1] = new DoubleRange ("x2",0.3, 0.0);
+                xyz [2] = new DoubleRange ("x3",0.3, 0.0);
+                xyz [3] = new DoubleRange ("x4",0.3, 0.0);
+                xyz [4] = new DoubleRange ("x5",0.3, 0.0);
 
-		// the objective constraints is a property of the problem. However, as a parameter
-		// of this module, we can either minimize or maximize by swapping the values
-		ObjectiveConstraints oc [] = new ObjectiveConstraints [2];
-		oc[0] = ObjectiveConstraintsFactory.getObjectiveConstraints ("Return",
-				.270, .190);
-		oc[1] = ObjectiveConstraintsFactory.getObjectiveConstraints ("Risk",
-				.0010, .0024);
-		NsgaPopulation pop = new ConstrainedNsgaPopulation (xyz, oc, this.getPopulationSize (), this.getTargetFitness ());
-		pop.setMaxGenerations (this.maxGenerations);
-		this.pushOutput (pop, 0);
-	}
+                // the objective constraints is a property of the problem. However, as a parameter
+                // of this module, we can either minimize or maximize by swapping the values
+                ObjectiveConstraints oc [] = new ObjectiveConstraints [2];
+                oc[0] = ObjectiveConstraintsFactory.getObjectiveConstraints ("Return",
+                                .270, .190);
+                oc[1] = ObjectiveConstraintsFactory.getObjectiveConstraints ("Risk",
+                                .0010, .0024);
+                NsgaPopulation pop = new ConstrainedNsgaPopulation (xyz, oc, this.getPopulationSize (), this.getTargetFitness (), 1);
+                pop.setMaxGenerations (this.maxGenerations);
+                this.pushOutput (pop, 0);
+        }
 
-	/**
-	 * Return the human readable name of the module.
-	 * @return the human readable name of the module.
-	 */
-	public String getModuleName() {
-		return "GenerateFinancialPopulation";
-	}
+        /**
+         * Return the human readable name of the module.
+         * @return the human readable name of the module.
+         */
+        public String getModuleName() {
+                return "GenerateFinancialPopulation";
+        }
 
-	/**
-	 * Return the human readable name of the indexed input.
-	 * @param index the index of the input.
-	 * @return the human readable name of the indexed input.
-	 */
-	public String getInputName(int index) {
-		switch(index) {
-			default: return "NO SUCH INPUT!";
-		}
-	}
+        /**
+         * Return the human readable name of the indexed input.
+         * @param index the index of the input.
+         * @return the human readable name of the indexed input.
+         */
+        public String getInputName(int index) {
+                switch(index) {
+                        default: return "NO SUCH INPUT!";
+                }
+        }
 
-	/**
-	 * Return the human readable name of the indexed output.
-	 * @param index the index of the output.
-	 * @return the human readable name of the indexed output.
-	 */
-	public String getOutputName(int index) {
-		switch(index) {
-			case 0:
-				return "Population";
-			default: return "NO SUCH OUTPUT!";
-		}
-	}
+        /**
+         * Return the human readable name of the indexed output.
+         * @param index the index of the output.
+         * @return the human readable name of the indexed output.
+         */
+        public String getOutputName(int index) {
+                switch(index) {
+                        case 0:
+                                return "Population";
+                        default: return "NO SUCH OUTPUT!";
+                }
+        }
 }
