d861bce3d6f822c549907d526fb9f8af974c7eb0
David Clutter <clutter@illinois.edu>
2004-02-10 14:44:35 +0000

    no message

---
+++ core/ncsa/d2k/modules/core/optimize/ga/emo/EMOFunction.java
@@ -0,0 +1,8 @@
+package ncsa.d2k.modules.core.optimize.ga.emo;
+
+public interface EMOFunction {
+  
+  public Property[] getProperties();  
+  public String getName();
+  public String getDescription();
+}
