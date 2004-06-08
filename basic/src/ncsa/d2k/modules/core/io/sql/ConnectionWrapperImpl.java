d832d7a670fe1635b639d2eddf65c439d982bff0
Miranda Callahan <mcallaha@illinois.edu>
2004-06-08 14:37:13 +0000

    changed JOptionPane to a local variable.  it was not serializing correctly, and was never accessed outside of a single try-catch block in getConnection in any case.

--- basic/src/ncsa/d2k/modules/core/io/sql/ConnectionWrapperImpl.java
+++ basic/src/ncsa/d2k/modules/core/io/sql/ConnectionWrapperImpl.java
@@ -9,90 +9,49 @@ import javax.swing.*;
 
 
 /**
-
   ConnectionWrapper is a wrapper to java.sql.Connection
-
   It is passed around between the modules in the
-
   ncsa.d2k.modules.sql package.  If the modules
-
   are all running on the same machine, the connection
-
   is shared.  If the ConnectionWrapper gets passed to
-
   another machine, a new connection is established
-
   using the info contained herein.
-
 */
 
 public class ConnectionWrapperImpl implements ConnectionWrapper, java.io.Serializable
 
 {
-
 	/** The connection. */
-
 	private transient Connection connection;
 
-
-
 	/** The url identifies the data source when establishing the connection. */
-
 	private String url;
 
-
-
 	/** The fully qualified classname of the JDBC driver needs to be known
-
 	  if the vm doesn't have the class loaded when attempting to establish
-
 	  the connecion. */
-
 	private String driver;
 
-
-
 	/** If username and password are not null the version of Driver.getConnection()
-
 	  that takes username and password arguments will be used. */
-
 	private String username;
 
 	private String password;
 
-        JOptionPane msgBoard = new JOptionPane();
-
-
-
 	public ConnectionWrapperImpl
-
 		(String _url, String _driver, String _username, String _password)
-
 	{
-
 		url = _url;
-
 		driver = _driver;
-
 		username = _username;
-
 		password = _password;
-
 	} /* ConnectionWrapper(String,String,String,String) */
 
-
-
-
-
-    public Connection getConnection()
-
-    {
-
-      try {
-
-        //System.out.println ("Entering getConnection");
-
-        if (connection == null) {
+        public Connection getConnection()
+        {
+          try {
+            //System.out.println ("Entering getConnection");
+            if (connection == null) {
 
           Class classNm = Class.forName (driver);
 
@@ -125,23 +84,14 @@ public class ConnectionWrapperImpl implements ConnectionWrapper, java.io.Seriali
       }
 
       catch (Exception e){
-
+        JOptionPane msgBoard = new JOptionPane();
         JOptionPane.showMessageDialog(msgBoard,
-
           "Cannot connect to the database. Please check your 'Connect To DB' "+
-
           "properties and verify that the appropriate driver is installed. ", "Error",
-
           JOptionPane.ERROR_MESSAGE);
-
         System.out.println("Error occurred when connecting to a database.");
-
         return null;
-
       }
-
-
-
     }
 
 
