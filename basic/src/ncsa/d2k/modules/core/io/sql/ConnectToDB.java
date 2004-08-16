09d1240a1266e1af8873b75f8eb5bf7f655c002f
Miranda Callahan <mcallaha@illinois.edu>
2004-08-16 15:09:21 +0000

    fixed it so the oracle port and driver show up immediately in the module; before, user had to switch to another module and then switch back to get the default port and driver to show up.

--- basic/src/ncsa/d2k/modules/core/io/sql/ConnectToDB.java
+++ basic/src/ncsa/d2k/modules/core/io/sql/ConnectToDB.java
@@ -586,66 +586,37 @@ public class ConnectToDB extends HeadlessUIModule {
         public void initView(ViewModule mod) {
 
             /**
-
              *** Initial Setup ***
-
              */
 
             JPanel placeholder = new JPanel();
-
             JPanel p = new JPanel();
-
             p.setLayout(new GridBagLayout());
-
             cbV = new JComboBox( Vendors );
-
             cbV.setEditable(false);
-
             cbV.setAlignmentX(Component.LEFT_ALIGNMENT);
-
             cbV.addActionListener(new ActionListener() {
 
                 public void actionPerformed(ActionEvent e) {
-
                     JComboBox cb = (JComboBox)e.getSource();
-
                     String newSelection = (String)cb.getSelectedItem();
-
                     if (newSelection == "Oracle") {
-
-                        tfPo.setText("1521");
-
-                        tfD.setText("oracle.jdbc.driver.OracleDriver");
-
+                      tfPo.setText("1521");
+                      tfD.setText("oracle.jdbc.driver.OracleDriver");
                     }
-
                     else if (newSelection == "MySQL") {
-
                         tfPo.setText("1520");
-
                         tfD.setText("com.mysql.jdbc.Driver");
-
                     }
-
                     else if (newSelection == "SQLServer") {
-
                         tfPo.setText("1433");
-
                         tfD.setText("com.microsoft.jdbc.sqlserver.SQLServerDriver");
-
                     }
-
                     else if (newSelection == "DB2") {
-
                         tfPo.setText("6789");
-
                         tfD.setText("COM.ibm.db2.jdbc.net.DB2Driver");
-
                     }
-
-
                 }
-
             }
 
             );
@@ -653,303 +624,156 @@ public class ConnectToDB extends HeadlessUIModule {
 
 
             /**
-
              *** Process Each Label: username, password, machine, port***
-
              ***                     dbInstance, driver               ***
-
              */
 
-
-
             // Add username
-
             // try to display the last username chosen
-
             String s = getUsername();
-
             if(s != null) {
-
                 tfU.setText(s);
-
             }
-
             boolean allPropsNull = true;
-
-
 
             // Add Database Vendors
             // try to display the last vendor chosen
-
             String v = getDbVendor();
-
             if(v != null) {
-
                 cbV.setSelectedItem(v);
-
             }
-
             JLabel lV = new JLabel("Vendor");
-
             Constrain.setConstraints(p, lV, 0, 0, 1, 1,
-
                                      GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 1, 1);
-
-
 
             Constrain.setConstraints(p, cbV, 1, 0, 2, 1,
-
                                      GridBagConstraints.HORIZONTAL,
-
                                      GridBagConstraints.WEST, 4, 1);
-
-
 
             // Add username
-
             JLabel lU = new JLabel("Username");
-
             Constrain.setConstraints(p, lU, 0, 1, 1, 1,
-
                                      GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 1, 1);
-
-
 
             // try to display the last username chosen
-
             s = getUsername();
-
-            if(s != null) {
-
+            if ((s != null) && (s.length()>0)){
                 tfU.setText(s);
-
                 allPropsNull = false;
-
             }
-
-
-
             Constrain.setConstraints(p, tfU, 1, 1, 2, 1,
-
                                      GridBagConstraints.HORIZONTAL,
-
                                      GridBagConstraints.WEST, 4, 1);
-
-
 
             // Add password
-
             JLabel lPa = new JLabel("Password");
-
             Constrain.setConstraints(p, lPa, 0, 2, 1, 1,
-
                                      GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 1, 1);
-
-
 
             // try to display the last password chosen
-
             s = getPassword();
-
-            if(s != null) {
-
+            if ((s != null) && (s.length()>0)){
                 pfPa.setText(s);
-
                 allPropsNull = false;
-
             }
-
-
-
             Constrain.setConstraints(p, pfPa, 1, 2, 2, 1,
-
                                      GridBagConstraints.HORIZONTAL,
-
                                      GridBagConstraints.WEST, 4, 1);
-
-
 
             // Add machine
-
             JLabel lM = new JLabel("Machine");
-
             Constrain.setConstraints(p, lM, 0, 3, 1, 1,
-
                                      GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 1, 1);
-
-
 
             // try to display the last machine chosen
-
             s = getMachine();
-
-            if(s != null) {
-
+            if ((s != null) && (s.length()>0)){
                 tfM.setText(s);
-
                 allPropsNull = false;
-
             }
-
             Constrain.setConstraints(p, tfM, 1, 3, 2, 1,
-
                                      GridBagConstraints.HORIZONTAL,
-
                                      GridBagConstraints.WEST, 4, 1);
-
-
 
             // Add port
-
             JLabel lPo = new JLabel("Port");
-
             Constrain.setConstraints(p, lPo, 0, 4, 1, 1,
-
                                      GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 1, 1);
-
-
 
             // try to display the last port chosen
-
             s = getPort();
-
-            if(s != null) {
-
+            if ((s != null) && (s.length()>0)){
                 tfPo.setText(s);
-
                 allPropsNull = false;
-
             }
-
             Constrain.setConstraints(p, tfPo, 1, 4, 2, 1,
-
                                      GridBagConstraints.HORIZONTAL,
-
                                      GridBagConstraints.WEST, 4, 1);
-
-
 
             // Add dbInstance
-
             JLabel ldbI = new JLabel("Database Instance");
-
             Constrain.setConstraints(p, ldbI, 0, 5, 1, 1,
-
                                      GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 1, 1);
-
-
 
             // try to display the last dbInstance chosen
-
             s = getDbInstance();
-
-            if(s != null) {
-
+            if ((s != null) && (s.length()>0)){
                 tfdbI.setText(s);
-
                 allPropsNull = false;
-
             }
-
-
 
             Constrain.setConstraints(p, tfdbI, 1, 5, 2, 1,
-
                                      GridBagConstraints.HORIZONTAL,
-
                                      GridBagConstraints.WEST, 4, 1);
-
-
 
             // Add driver
-
             JLabel lD = new JLabel("Driver");
-
             Constrain.setConstraints(p, lD, 0, 6, 1, 1,
-
                                      GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 1, 1);
 
-
-
             // try to display the last driver chosen
-
             s = getDriver();
-
-
-
-            if(s != null) {
-
+            if ((s != null) && (s.length()>0)){
                 tfD.setText(s);
-
                 allPropsNull = false;
-
             }
 
-
-
             Constrain.setConstraints(p, tfD, 1, 6, 2, 1,
-
                                      GridBagConstraints.HORIZONTAL,
-
-                                     GridBagConstraints.WEST, 4, 1);
-
-
-
-
-
-
-            Constrain.setConstraints(p, placeholder, 0, 4, 4, 1,
-
-                                     GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 4, 1);
 
             Constrain.setConstraints(p, placeholder, 0, 4, 4, 1,
-
                                      GridBagConstraints.NONE,
-
                                      GridBagConstraints.WEST, 4, 1);
 
-
+            Constrain.setConstraints(p, placeholder, 0, 4, 4, 1,
+                                     GridBagConstraints.NONE,
+                                     GridBagConstraints.WEST, 4, 1);
 
             bAb.addActionListener(this);
-
             bDo.addActionListener(this);
 
-
-
             if(allPropsNull) {
-
                 cbV.setSelectedItem("Oracle");
-
+                // MRC ADDED august 13 04 to fill in slots on first look for
+                // oracle db
+                tfPo.setText("1521");
+                tfD.setText("oracle.jdbc.driver.OracleDriver");
             }
 
             setLayout(new BorderLayout());
-
             add(p, BorderLayout.CENTER);
-
             JPanel buttonPanel = new JPanel();
-
             buttonPanel.add(bAb);
-
             buttonPanel.add(bDo);
-
             add(buttonPanel, BorderLayout.SOUTH);
-
         }
 
 
@@ -1122,7 +946,7 @@ public class ConnectToDB extends HeadlessUIModule {
 
 
 //	headless conversion support
-		
+
 		  public void doit() throws Exception{
 
 
@@ -1150,7 +974,7 @@ public class ConnectToDB extends HeadlessUIModule {
 						  throw new Exception(getAlias() + " has not been configured. Before running " +
 											  "headless configure the properties via running with GUI.");
 
-											  
+
 			if ( getDriver().equals("oracle.jdbc.driver.OracleDriver")) {
 
 				setUrl("jdbc:oracle:thin:@"+getMachine()+":"+getPort()+":"+getDbInstance());
@@ -1218,6 +1042,6 @@ public class ConnectToDB extends HeadlessUIModule {
 
 		  }
 
-			
-     
+
+
 }
