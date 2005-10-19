e6bcef85099a2245d328b2c783b36e43c838bd6e
David Clutter <clutter@illinois.edu>
2005-10-19 16:43:21 +0000

    Updated to implement the getTableFactory() method in the Table interface

--- basic/src/ncsa/d2k/modules/core/discovery/cluster/ClusterModel.java
+++ basic/src/ncsa/d2k/modules/core/discovery/cluster/ClusterModel.java
@@ -36,136 +36,136 @@ public class ClusterModel
   private Table _table = null;
   private ArrayList _clusters = null;
   private TableCluster _root = null;
-	
+
   /** this is the missing value for longs, ints, and shorts. */
   protected int defaultMissingInt = 0;
-	
+
   /** default for float double and extended. */
   protected double defaultMissingDouble = 0.0;
-	
+
   /** default missing string. */
   protected String defaultMissingString = "?";
-	
+
   /** default missing string. */
   protected boolean defaultMissingBoolean = false;
-	
+
   /** default missing string. */
   protected char[] defaultMissingCharArray = {'\000'};
-	
+
   /** default missing string. */
   protected byte[] defaultMissingByteArray = {(byte)'\000'};
-	
+
   /** default missing string. */
   protected char defaultMissingChar = '\000';
-	
+
   /** default missing string. */
   protected byte defaultMissingByte = (byte)'\000';
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @returns the integer for missing value.
    */
   public int getMissingInt () {
 	  return defaultMissingInt;
   }
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @param the integer for missing values.
    */
   public void setMissingInt (int newMissingInt) {
 	  this.defaultMissingInt = newMissingInt;
   }
-	
+
   /** return the default missing value for doubles, floats and extendeds.
    * @returns the double for missing value.
    */
   public double getMissingDouble () {
 	  return this.defaultMissingDouble;
   }
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @param the integer for missing values.
    */
   public void setMissingDouble (double newMissingDouble) {
 	  this.defaultMissingDouble = newMissingDouble;
   }
-	
+
   /** return the default missing value for doubles, floats and extendeds.
    * @returns the double for missing value.
    */
   public String getMissingString () {
 	  return this.defaultMissingString;
   }
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @param the integer for missing values.
    */
   public void setMissingString (String newMissingString) {
 	  this.defaultMissingString = newMissingString;
   }
-	
+
   /** return the default missing value for doubles, floats and extendeds.
    * @returns the double for missing value.
    */
   public boolean getMissingBoolean() {
 	  return defaultMissingBoolean;
   }
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @param the integer for missing values.
    */
   public void setMissingBoolean(boolean newMissingBoolean) {
 	  this.defaultMissingBoolean = newMissingBoolean;
   }
-	
+
   /** return the default missing value for doubles, floats and extendeds.
    * @returns the double for missing value.
    */
   public char[] getMissingChars() {
 	  return this.defaultMissingCharArray;
   }
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @param the integer for missing values.
    */
   public void setMissingChars(char[] newMissingChars) {
 	  this.defaultMissingCharArray = newMissingChars;
   }
-	
+
   /** return the default missing value for doubles, floats and extendeds.
    * @returns the double for missing value.
    */
   public byte[] getMissingBytes() {
 	  return this.defaultMissingByteArray;
   }
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @param the integer for missing values.
    */
   public void setMissingBytes(byte[] newMissingBytes) {
 	  this.defaultMissingByteArray = newMissingBytes;
   }
-	
+
   /** return the default missing value for doubles, floats and extendeds.
    * @returns the double for missing value.
    */
   public char getMissingChar() {
 	  return this.defaultMissingChar;
   }
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @param the integer for missing values.
    */
   public void setMissingChar(char newMissingChar) {
 	  this.defaultMissingChar = newMissingChar;
   }
-	
+
   /** return the default missing value for doubles, floats and extendeds.
    * @returns the double for missing value.
    */
   public byte getMissingByte() {
 	  return defaultMissingByte;
   }
-	
+
   /** return the default missing value for integers, both short, int and long.
    * @param the integer for missing values.
    */
@@ -658,7 +658,7 @@ public class ClusterModel
    * See Table in ncsa.d2k.modules.core.datatype.table
    */
 // ANCA: changed to the new interface
- 
+
    public Column getColumn(int position) {
     if (_table == null) {
       System.out.println(
@@ -667,7 +667,7 @@ public class ClusterModel
     } else {
      return  _table.getColumn( position);
     }
-  } 
+  }
 
   /**
    * See Table in ncsa.d2k.modules.core.datatype.table
@@ -692,7 +692,7 @@ public class ClusterModel
       return _table.copy();
     }
   }
-  
+
   //ANCA: added method below
   /**
    * Create a copy of this Table. This is a deep copy, and it contains a copy of
@@ -727,7 +727,7 @@ public class ClusterModel
   /**
    * See Table in ncsa.d2k.modules.core.datatype.table
    */
- /*ANCA public TableFactory getTableFactory() {
+ public TableFactory getTableFactory() {
     if (_table == null) {
       System.out.println("ERROR: " + getAlias() + ".getTableFactory() -- Model does not contain table data -- will return null");
       return null;
@@ -735,7 +735,7 @@ public class ClusterModel
       return _table.getTableFactory();
     }
   }
-*/
+
   /**
    * See Table in ncsa.d2k.modules.core.datatype.table
    */
@@ -904,7 +904,7 @@ public class ClusterModel
       return _table.hasMissingValues();
     }
   }
-  
+
   //ANCA added method below
   public MutableTable createTable() {
 	return new MutableTableImpl().createTable();
