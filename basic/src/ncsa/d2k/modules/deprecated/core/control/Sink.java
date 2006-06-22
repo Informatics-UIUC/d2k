c43a9949ce160a54cd5f253861cf20e7de88698e
Andrew Shirk <shirk@illinois.edu>
2006-06-22 20:23:16 +0000

    Moved to deprecated.

---
+++ basic/src/ncsa/d2k/modules/deprecated/core/control/Sink.java
@@ -0,0 +1,110 @@
+/*
+ * Created on Dec 14, 2004
+ * Copyright 2004 University of Illinois Board of Trustees.
+ */
+package ncsa.d2k.modules.deprecated.core.control;
+
+import ncsa.d2k.core.modules.OutputModule;
+
+/**
+ * @author redman
+ */
+public class Sink extends OutputModule {
+	
+    /**
+     * Provides notification that this module is deprecated.
+     * 
+     * @author shirk
+     */
+    public void beginExecution() {
+    	System.out.println(getClass().getName() + " is deprecated.");
+    	super.beginExecution();
+    }
+    
+	/**
+		This method returns the description of the various inputs.
+		@returns the description of the indexed input.
+	*/
+	public String getInputInfo (int index) {
+		switch (index) {
+			case 0: return "This is the object to be trashed. ";
+			default: return "No such input";
+		}
+	}
+	
+	/**
+		This method returns an array of strings that contains the data types for the inputs.
+		@returns the data types of all inputs.
+	*/
+	public String[] getInputTypes () {
+		String[] types = {"java.lang.Object"};
+		return types;
+	}
+	
+	/**
+		This method returns the description of the outputs.
+		@returns the description of the indexed output.
+	*/
+	public String getOutputInfo (int index) {
+		switch (index) {
+			default: return "No such output";
+		}
+	}
+	
+	/**
+		This method returns an array of strings that contains the data types for the outputs.
+		@returns the data types of all outputs.
+	*/
+	public String[] getOutputTypes () {
+		String[] types = {		};
+		return types;
+	}
+	
+	/**
+		This method returns the description of the module.
+		@returns the description of the module.
+	*/
+	public String getModuleInfo () {
+		return "<html><head></head>  <body>Trash any inputs received</body></html>";
+	}
+	
+	/**
+		@param outV the array to contain output object.
+	*/
+	public void doit () throws Exception {
+		this.pullInput (0);
+	}
+	
+	/**
+	 * Return the human readable name of the module.
+	 * @return the human readable name of the module.
+	 */
+	public String getModuleName() {
+		return "Trash Object";
+	}
+	
+	/**
+	 * Return the human readable name of the indexed input.
+	 * @param index the index of the input.
+	 * @return the human readable name of the indexed input.
+	 */
+	public String getInputName(int index) {
+		switch(index) {
+			case 0:
+				return "Trashed Object";
+			default: return "NO SUCH INPUT!";
+		}
+	}
+	
+	/**
+	 * Return the human readable name of the indexed output.
+	 * @param index the index of the output.
+	 * @return the human readable name of the indexed output.
+	 */
+	public String getOutputName(int index) {
+		switch(index) {
+			default: return "NO SUCH OUTPUT!";
+		}
+	}
+
+}
