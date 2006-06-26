045e474d197234848494b25c8ec41b5e2767b3b9
Andrew Shirk <shirk@illinois.edu>
2006-06-26 19:52:30 +0000

    Small doc fix

--- basic/src/ncsa/d2k/modules/core/prediction/EvaluateModels.java
+++ basic/src/ncsa/d2k/modules/core/prediction/EvaluateModels.java
@@ -15,27 +15,25 @@ import ncsa.d2k.modules.core.datatype.table.*;
                capable of receiving multiple models. they are all tested on the
                same test data, and make it easy to compare performances.
 */
-public class  EvaluateModels extends ncsa.d2k.modules.core.prediction.ModelPredict
-{
+public class  EvaluateModels extends ncsa.d2k.modules.core.prediction.ModelPredict {
 
-
-public String getModuleName() {
-    return "Evaluate Models";
-  }
+    public String getModuleName() {
+        return "Evaluate Models";
+    }
 
 	/**
 		This pair returns the description of the module.
 		@return the description of the module.
 	*/
 	public String getModuleInfo() {
-		StringBuffer sb = new StringBuffer( "Overview: This module applies a prediction model to a table of examples and ");
-		sb.append("makes predictions for each output attribute based on the values of the input attributes. ");
+		StringBuffer sb = new StringBuffer( "<p>Overview: This module applies a prediction model to a table of examples and ");
+		sb.append("makes predictions for each output attribute based on the values of the input attributes.</p>");
 
-		sb.append("</p><p>Detailed Description:  This module applies a previously built model to a new set of examples that have the ");
-		sb.append("same attributes as those used to train/build the model.  The module creates a new table that contains ");
+		sb.append("<p>Detailed Description: This module applies a previously built model to a new set of examples that have the ");
+		sb.append("same attributes as those used to train/build the model. The module creates a new table that contains ");
 		sb.append("columns for each of the values the model predicts, in addition to the columns found in the original table. ");
 		sb.append("The new columns are filled in with values predicted by the model based on the values of the input attributes. ");
-                sb.append("The module is capable of testing severals models on the same test data. ");
+                sb.append("The module is capable of testing severals models on the same test data.</p>");
 
 		return sb.toString();
 	}
