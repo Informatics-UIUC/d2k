56a9957ebba056eedfb6662255c8beb6766722f7
Robert McGrath <mcgrath@illinois.edu>
2006-07-25 18:26:51 +0000

    Format and comment clean up.

--- basic/src/ncsa/d2k/modules/core/control/Branch.java
+++ basic/src/ncsa/d2k/modules/core/control/Branch.java
@@ -1,82 +1,134 @@
+/* 
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ * 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ * 
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.control;
 
 import ncsa.d2k.core.modules.DataPrepModule;
 import ncsa.d2k.core.modules.PropertyDescription;
 
+
 /**
- * Created by IntelliJ IDEA.
- * User: clutter
- * Date: May 5, 2006
- * Time: 1:42:53 PM
- * To change this template use File | Settings | File Templates.
+ * A simple branch: select output according to <b>useFirstOutput</b>.
+ *
+ * <p>Input an object, output it to one of two outputs, selected by the <b>
+ * useFirstOutput</b>.</p>
+ *
+ * @author  $Author$
+ * @version $Revision$, $Date$
  */
 public class Branch extends DataPrepModule {
-    private boolean useFirstOutput = true;
-    public void setUseFirstOutput(boolean b) {
-        useFirstOutput = b;
-    }
-    public boolean getUseFirstOutput() {
-        return useFirstOutput;
-    }
 
-    public String getModuleName() {
-       return "Branch";
-    }
-    
-    /**
-     * put your documentation comment here
-     * @return
-     */
-    public PropertyDescription[] getPropertiesDescriptions () {
-        PropertyDescription[] desc = new PropertyDescription[1];
-        desc[0] = new PropertyDescription("useFirstOutput",
-                "Use the first output",
-                "Set this to true to use the first output. Set this to false "+
-                "to use the second output.");
-        return  desc;
-    }
+   //~ Instance fields ************************************************
 
-    public String[] getInputTypes() {
-        return new String[] {
-            "java.lang.Object"
-        };
-    }
+   /** if useFirstOutput == true, push the first, else push the second. */
+   private boolean useFirstOutput = true;
 
-    public String[] getOutputTypes() {
-        return new String[] {
-            "java.lang.Object",
-            "java.lang.Object"
-        } ;
-    }
+   //~ Methods ******************************************************
 
-    public String getInputInfo(int i) {
-        return "Object";
-    }
+   /**
+    * Input one object, output it to one of two outputs selected by <b>
+    * useFirstOutput</b>.
+    *
+    * @throws Exception Description of exception Exception.
+    */
+   public void doit() throws Exception {
+      Object o = pullInput(0);
 
-    public String getOutputInfo(int i) {
-        return "Object";
-    }
+      if (useFirstOutput) {
+         pushOutput(o, 0);
+      } else {
+         pushOutput(o, 1);
+      }
+   }
 
-    public String getInputName(int i) {
-      return "Object";
-    }
+   
+   public String getInputInfo(int i) { return "Object"; }
 
-    public String getOutputName(int i) {
-      return "Object";
-    }
+   public String getInputName(int i) { return "Object"; }
 
-    public String getModuleInfo() {
-        return "<p>Overview: Simple branch. Pushes data out the first output pipe if <i>useFirstOutput</i> is"+
-                " true. Pushes data out the second output pipe if <i>useFirstOutput</i> is "+
-                "false.</p>";
-    }
+   public String[] getInputTypes() { return new String[] { "java.lang.Object" }; }
 
-    public void doit() throws Exception {
-        Object o = pullInput(0);
+   public String getModuleInfo() {
+      return "<p>Overview: Simple branch. Pushes data out the first output pipe" +
+      		" if <i>useFirstOutput</i> is" +
+             " true. Pushes data out the second output pipe if " +
+             "<i>useFirstOutput</i> is " +
+             "false.</p>";
 
-        if(useFirstOutput)
-            pushOutput(o, 0);
-        else
-            pushOutput(o, 1);
-    }
-}
+   }
+
+   public String getModuleName() { return "Branch"; }
+
+   public String getOutputInfo(int i) { return "Object"; }
+
+   public String getOutputName(int i) { return "Object"; }
+
+   public String[] getOutputTypes() { return new String[] {
+                                                "java.lang.Object",
+                                                "java.lang.Object"
+                                             }; }
+
+   public PropertyDescription[] getPropertiesDescriptions() {
+      PropertyDescription[] desc = new PropertyDescription[1];
+      desc[0] =
+         new PropertyDescription("useFirstOutput",
+                                 "Use the first output",
+                                 "Set this to true to use the first " +
+                                 "output. Set this to false " +
+                                 "to use the second output.");
+
+      return desc;
+   }
+
+   public boolean getUseFirstOutput() { return useFirstOutput; }
+
+   /**
+    * Select first (<b>true</b>) or second.
+    * <p>
+    * Initially set to <b>true</b>.
+    *
+    * @param b true = output first, else second
+    */
+   public void setUseFirstOutput(boolean b) { useFirstOutput = b; }
+} // end class Branch
