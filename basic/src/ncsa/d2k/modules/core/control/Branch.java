fb7d820b5fd6862832b0b8a3991232d55d7466b2
Andrew Shirk <shirk@illinois.edu>
2006-07-26 18:57:30 +0000

    Completed documentation improvements

--- basic/src/ncsa/d2k/modules/core/control/Branch.java
+++ basic/src/ncsa/d2k/modules/core/control/Branch.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * $Header$
  *
  * ===================================================================
@@ -6,17 +6,17 @@
  * D2K-Workflow
  * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
  * ILLINOIS. All rights reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License v2.0
  * as published by the Free Software Foundation and with the required
  * interpretation regarding derivative works as described below.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License v2.0 for more details.
- * 
+ *
  * This program and the accompanying materials are made available
  * under the terms of the GNU General Public License v2.0 (GPL v2.0)
  * which accompanies this distribution and is available at
@@ -34,7 +34,7 @@
  * make those components a derivative work of D2K-Workflow.
  * (Examples of such independently developed components include for
  * example, external databases or metadata and provenance stores).
- * 
+ *
  * Note: A non-GPL commercially licensed version of contributions
  * from the UNIVERSITY OF ILLINOIS may be available from the
  * designated commercial licensee RiverGlass, Inc. located at
@@ -49,28 +49,32 @@ import ncsa.d2k.core.modules.PropertyDescription;
 
 
 /**
- * A simple branch: select output according to <b>useFirstOutput</b>.
+ * A simple branch: select output according to <code>useFirstOutput</code>.
  *
- * <p>Input an object, output it to one of two outputs, selected by the <b>
- * useFirstOutput</b>.</p>
+ * <p>Input an object, output it to one of two outputs, selected by the <code>
+ * useFirstOutput</code>.</p>
  *
+ * @author  clutter
+ * @author  gpape
  * @author  $Author$
  * @version $Revision$, $Date$
  */
 public class Branch extends DataPrepModule {
 
-   //~ Instance fields ************************************************
+   //~ Instance fields *********************************************************
 
-   /** if useFirstOutput == true, push the first, else push the second. */
+   /** If useFirstOutput == true, push the first, else push the second. */
    private boolean useFirstOutput = true;
 
-   //~ Methods ******************************************************
+   //~ Methods *****************************************************************
 
    /**
-    * Input one object, output it to one of two outputs selected by <b>
-    * useFirstOutput</b>.
+    * Performs the main work of the module. In this case, one object is input,
+    * then directed to one of two outputs selected using <code>
+    * useFirstOutput</code>.
     *
-    * @throws Exception Description of exception Exception.
+    * @throws Exception if a problem occurs while performing the work of the
+    *                   module.
     */
    public void doit() throws Exception {
       Object o = pullInput(0);
@@ -82,33 +86,95 @@ public class Branch extends DataPrepModule {
       }
    }
 
-   
+   /**
+    * Returns a description of the input at the specified index.
+    *
+    * @param  i Index of the input for which a description should be returned.
+    *
+    * @return <code>String</code> describing the input at the specified index.
+    */
    public String getInputInfo(int i) { return "Object"; }
 
+   /**
+    * Returns the name of the input at the specified index.
+    *
+    * @param  i Index of the input for which a name should be returned.
+    *
+    * @return <code>String</code> containing the name of the input at the
+    *         specified index.
+    */
    public String getInputName(int i) { return "Object"; }
 
+   /**
+    * Returns an array of <code>String</code> objects each containing the fully
+    * qualified Java data type of the input at the corresponding index.
+    *
+    * @return An array of <code>String</code> objects each containing the fully
+    *         qualified Java data type of the input at the corresponding index.
+    */
    public String[] getInputTypes() { return new String[] { "java.lang.Object" }; }
 
+   /**
+    * Describes the purpose of the module.
+    *
+    * @return <code>String</code> describing the purpose of the module.
+    */
    public String getModuleInfo() {
-      return "<p>Overview: Simple branch. Pushes data out the first output pipe" +
-      		" if <i>useFirstOutput</i> is" +
+      return "<p>Overview: Simple branch. Pushes data out the first output " +
+             "pipe" +
+             " if <i>useFirstOutput</i> is" +
              " true. Pushes data out the second output pipe if " +
              "<i>useFirstOutput</i> is " +
              "false.</p>";
 
    }
 
+   /**
+    * Returns the name of the module that is appropriate for end user
+    * consumption.
+    *
+    * @return The name of the module.
+    */
    public String getModuleName() { return "Branch"; }
 
+   /**
+    * Returns a description of the output at the specified index.
+    *
+    * @param  i Index of the output for which a description should be returned.
+    *
+    * @return <code>String</code> describing the output at the specified index.
+    */
    public String getOutputInfo(int i) { return "Object"; }
 
+   /**
+    * Returns the name of the output at the specified index.
+    *
+    * @param  i Index of the output for which a description should be returned.
+    *
+    * @return <code>String</code> containing the name of the output at the
+    *         specified index.
+    */
    public String getOutputName(int i) { return "Object"; }
 
+   /**
+    * Returns an array of <code>String</code> objects each containing the fully
+    * qualified Java data type of the output at the corresponding index.
+    *
+    * @return An array of <code>String</code> objects each containing the fully
+    *         qualified Java data type of the output at the corresponding index.
+    */
    public String[] getOutputTypes() { return new String[] {
                                                 "java.lang.Object",
                                                 "java.lang.Object"
                                              }; }
 
+   /**
+    * Returns an array of <code>ncsa.d2k.core.modules.PropertyDescription</code>
+    * objects for each property of the module.
+    *
+    * @return An array of <code>ncsa.d2k.core.modules.PropertyDescription</code>
+    *         objects.
+    */
    public PropertyDescription[] getPropertiesDescriptions() {
       PropertyDescription[] desc = new PropertyDescription[1];
       desc[0] =
@@ -121,12 +187,17 @@ public class Branch extends DataPrepModule {
       return desc;
    }
 
+   /**
+    * Returns whether or not the first output will be sent the object.
+    *
+    * @return Whether or not the first output will be sent the object
+    */
    public boolean getUseFirstOutput() { return useFirstOutput; }
 
    /**
     * Select first (<b>true</b>) or second.
-    * <p>
-    * Initially set to <b>true</b>.
+    *
+    * <p>Initially set to <b>true</b>.</p>
     *
     * @param b true = output first, else second
     */
--- basic/src/ncsa/d2k/modules/core/control/DisposeObject.java
+++ basic/src/ncsa/d2k/modules/core/control/DisposeObject.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * $Header$
  *
  * ===================================================================
@@ -6,17 +6,17 @@
  * D2K-Workflow
  * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
  * ILLINOIS. All rights reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License v2.0
  * as published by the Free Software Foundation and with the required
  * interpretation regarding derivative works as described below.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License v2.0 for more details.
- * 
+ *
  * This program and the accompanying materials are made available
  * under the terms of the GNU General Public License v2.0 (GPL v2.0)
  * which accompanies this distribution and is available at
@@ -34,7 +34,7 @@
  * make those components a derivative work of D2K-Workflow.
  * (Examples of such independently developed components include for
  * example, external databases or metadata and provenance stores).
- * 
+ *
  * Note: A non-GPL commercially licensed version of contributions
  * from the UNIVERSITY OF ILLINOIS may be available from the
  * designated commercial licensee RiverGlass, Inc. located at
@@ -42,7 +42,6 @@
  * ===================================================================
  *
  */
-
 package ncsa.d2k.modules.core.control;
 
 import ncsa.d2k.core.modules.OutputModule;
@@ -50,23 +49,34 @@ import ncsa.d2k.core.modules.OutputModule;
 
 /**
  * Input is read, no output is generated.
- * 
- * <p>In this module, the object is discarded.
+ *
+ * <p>In this module, the object is discarded.</p>
  *
  * @author  redman
+ * @author  $Author$
  * @version $Revision$, $Date$
  */
 public class DisposeObject extends OutputModule {
 
-   //~ Methods *******************************************************
+   //~ Methods *****************************************************************
 
    /**
-    * Discard one input.
+    * Performs the main work of the module. In this case, the input <code>
+    * Object</code> is simply discarded.
     *
-    * @throws Exception Description of exception Exception.
+    * @throws Exception if a problem occurs while performing the work of the
+    *                   module.
     */
    public void doit() throws Exception { this.pullInput(0); }
 
+   /**
+    * Returns a description of the input at the specified index.
+    *
+    * @param  index Index of the input for which a description should be
+    *               returned.
+    *
+    * @return <code>String</code> describing the input at the specified index.
+    */
    public String getInputInfo(int index) {
 
       switch (index) {
@@ -79,6 +89,14 @@ public class DisposeObject extends OutputModule {
       }
    }
 
+   /**
+    * Returns the name of the input at the specified index.
+    *
+    * @param  index Index of the input for which a name should be returned.
+    *
+    * @return <code>String</code> containing the name of the input at the
+    *         specified index.
+    */
    public String getInputName(int index) {
 
       switch (index) {
@@ -91,19 +109,45 @@ public class DisposeObject extends OutputModule {
       }
    }
 
+   /**
+    * Returns an array of <code>String</code> objects each containing the fully
+    * qualified Java data type of the input at the corresponding index.
+    *
+    * @return An array of <code>String</code> objects each containing the fully
+    *         qualified Java data type of the input at the corresponding index.
+    */
    public String[] getInputTypes() {
       String[] types = { "java.lang.Object" };
 
       return types;
    }
 
+   /**
+    * Describes the purpose of the module.
+    *
+    * @return <code>String</code> describing the purpose of the module.
+    */
    public String getModuleInfo() {
       return "<p>Overview: Disposes of any input received.</p>";
    }
 
+   /**
+    * Returns the name of the module that is appropriate for end user
+    * consumption.
+    *
+    * @return The name of the module.
+    */
    public String getModuleName() { return "Dispose Object"; }
 
 
+   /**
+    * Returns a description of the output at the specified index.
+    *
+    * @param  index Index of the output for which a description should be
+    *               returned.
+    *
+    * @return <code>String</code> describing the output at the specified index.
+    */
    public String getOutputInfo(int index) {
 
       switch (index) {
@@ -113,6 +157,15 @@ public class DisposeObject extends OutputModule {
       }
    }
 
+   /**
+    * Returns the name of the output at the specified index.
+    *
+    * @param  index Index of the output for which a description should be
+    *               returned.
+    *
+    * @return <code>String</code> containing the name of the output at the
+    *         specified index.
+    */
    public String getOutputName(int index) {
 
       switch (index) {
@@ -122,6 +175,13 @@ public class DisposeObject extends OutputModule {
       }
    }
 
+   /**
+    * Returns an array of <code>String</code> objects each containing the fully
+    * qualified Java data type of the output at the corresponding index.
+    *
+    * @return An array of <code>String</code> objects each containing the fully
+    *         qualified Java data type of the output at the corresponding index.
+    */
    public String[] getOutputTypes() {
       String[] types = {};
 
