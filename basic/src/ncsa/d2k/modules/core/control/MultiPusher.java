f7923115b0389e5a38fed9091d872554357eec1e
Andrew Shirk <shirk@illinois.edu>
2006-07-26 19:58:51 +0000

    Completed documentation improvements

--- basic/src/ncsa/d2k/modules/core/control/MultiPusher.java
+++ basic/src/ncsa/d2k/modules/core/control/MultiPusher.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * $Header$
  *
  * ===================================================================
@@ -6,17 +6,17 @@
  * D2K-Workflow
  * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
  * ILLINOIS. All rights reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License v2.0
  * as published by the Free Software Foundation and with the required
  * interpretation regarding derivative works as described below.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License v2.0 for more details.
- * 
+ *
  * This program and the accompanying materials are made available
  * under the terms of the GNU General Public License v2.0 (GPL v2.0)
  * which accompanies this distribution and is available at
@@ -34,7 +34,7 @@
  * make those components a derivative work of D2K-Workflow.
  * (Examples of such independently developed components include for
  * example, external databases or metadata and provenance stores).
- * 
+ *
  * Note: A non-GPL commercially licensed version of contributions
  * from the UNIVERSITY OF ILLINOIS may be available from the
  * designated commercial licensee RiverGlass, Inc. located at
@@ -48,52 +48,57 @@ import ncsa.d2k.core.modules.PropertyDescription;
 
 
 /**
- * Push the input object out N times, where N is a property
- * or input.
+ * Pushes the input object out N times, where N is a property or input.
  *
  * @author  Peter Groves 7/15/01 Revised 04/06/03 by pgroves
+ * @author  $Author$
  * @version $Revision$, $Date$
  */
 public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
    implements java.io.Serializable {
 
-   //~ Instance fields ***********************************************
+   //~ Static fields/initializers **********************************************
+
+   /** The universal version identifier. */
+   static private final long serialVersionUID = -5817833901849072719L;
+
+   //~ Instance fields *********************************************************
 
    /**
-    * false- will wait for and use the Integer in input 1 true - 
-    * will use the N in properties.
+    * Whether or not to use the value from field 'N' for determining how many
+    * times to push the object. If this field is false, will wait for and use
+    * the Integer in input 1. If this field is true, will use the N in
+    * properties.
     */
    private boolean usePropNValue = false;
 
+   /** Whether or not to print debug info to the console. */
    boolean debug = false;
-   
 
-   //////////////////////
-   // d2k Props
-   ////////////////////
-
-   /** the number of times to push the object. */
+   /** Number of times to push the object. */
    int N = 4;
 
    /**
-    * the current number of times this module has fired, indicating the number
-    * of times the input object has been pushed.
+    * Current number of times this module has fired, indicating the number of
+    * times the input object has been pushed.
     */
    int numFires = 0;
 
-   /** the object that is input (pulled in). */
+   /** The object that is input (pulled in). */
    Object obj;
 
    /**
-    * the total number of times this module has fired since the itin began, this
-    * is used only for debugging.
+    * Total number of times this module has fired since the itin began, this is
+    * used only for debugging.
     */
    int totalFires = 0;
 
-   //~ Methods ********************************************************
+   //~ Methods *****************************************************************
 
    /**
-    * Initialize the counters and call the superclass.
+    * Called by the D2K Infrastructure before the itinerary begins to execute.
+    * <In this case, initializes the counters and calls beginExecution() on the
+    * superclass.
     */
    public void beginExecution() {
       numFires = 0;
@@ -101,13 +106,12 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
       super.beginExecution();
    }
 
-   /////////////////////
-   // work methods
-   ////////////////////
-
    /**
-    * Input one object, output it once for each of N calls to doit.
+    * Performs the main work of the module. In this case, input one object,
+    * output it once for each of N calls to doit.
     *
+    * @throws Exception if a problem occurs while performing the work of the
+    *                   module.
     */
    public void doit() throws Exception {
 
@@ -125,9 +129,9 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
       totalFires++;
 
       if (debug) {
-         System.out.println(this.getAlias() + " current numFires:" + 
-        		 numFires +
-        		 "/" + N + ", total number fires this execution:" +
+         System.out.println(this.getAlias() + " current numFires:" +
+                            numFires +
+                            "/" + N + ", total number fires this execution:" +
                             totalFires);
       }
 
@@ -135,10 +139,23 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
          obj = null;
          numFires = 0;
       }
-   }
+   } // end method doit
 
+   /**
+    * Gets the debug value.
+    *
+    * @return Whether or not debug is turned on.
+    */
    public boolean getDebug() { return debug; }
 
+   /**
+    * Returns a description of the input at the specified index.
+    *
+    * @param  index Index of the input for which a description should be
+    *               returned.
+    *
+    * @return <code>String</code> describing the input at the specified index.
+    */
    public String getInputInfo(int index) {
 
       switch (index) {
@@ -148,7 +165,7 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
 
          case 1:
             return "The number of times to pass the input object. " +
-            		"This module" +
+                   "This module" +
                    " must receive exactly one Integer object for " +
                    "every object" +
                    " passed to the first input (unless the " +
@@ -161,6 +178,14 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
       }
    }
 
+   /**
+    * Returns the name of the input at the specified index.
+    *
+    * @param  index Index of the input for which a name should be returned.
+    *
+    * @return <code>String</code> containing the name of the input at the
+    *         specified index.
+    */
    public String getInputName(int index) {
 
       switch (index) {
@@ -176,27 +201,48 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
       }
    }
 
+   /**
+    * Returns an array of <code>String</code> objects each containing the fully
+    * qualified Java data type of the input at the corresponding index.
+    *
+    * @return An array of <code>String</code> objects each containing the fully
+    *         qualified Java data type of the input at the corresponding index.
+    */
    public String[] getInputTypes() {
       String[] types = { "java.lang.Object", "java.lang.Integer" };
 
       return types;
    }
 
+   /**
+    * Describes the purpose of the module.
+    *
+    * @return <code>String</code> describing the purpose of the module.
+    */
    public String getModuleInfo() {
       return "<p>Overview: Takes any object, pushes it out N times, " +
-      		"where N is either set" +
+             "where N is either set" +
              " as a property or passed in as an input. See " +
              "property descriptions " +
              " for details on controlling this behavior.</p>";
    }
 
 
-   ////////////////////////
-   /// D2K Info Methods
-   /////////////////////
-
+   /**
+    * Returns the name of the module that is appropriate for end user
+    * consumption.
+    *
+    * @return The name of the module.
+    */
    public String getModuleName() { return "MultiPusher"; }
 
+   /**
+    * Description of method getOutputInfo.
+    *
+    * @param  index Description of parameter index.
+    *
+    * @return Description of return value.
+    */
    public String getOutputInfo(int index) {
 
       switch (index) {
@@ -209,6 +255,15 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
       }
    }
 
+   /**
+    * Returns the name of the output at the specified index.
+    *
+    * @param  index Index of the output for which a description should be
+    *               returned.
+    *
+    * @return <code>String</code> containing the name of the output at the
+    *         specified index.
+    */
    public String getOutputName(int index) {
 
       switch (index) {
@@ -221,12 +276,26 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
       }
    }
 
+   /**
+    * Returns an array of <code>String</code> objects each containing the fully
+    * qualified Java data type of the output at the corresponding index.
+    *
+    * @return An array of <code>String</code> objects each containing the fully
+    *         qualified Java data type of the output at the corresponding index.
+    */
    public String[] getOutputTypes() {
       String[] types = { "java.lang.Object" };
 
       return types;
    }
 
+   /**
+    * Returns an array of <code>ncsa.d2k.core.modules.PropertyDescription</code>
+    * objects for each property of the module.
+    *
+    * @return An array of <code>ncsa.d2k.core.modules.PropertyDescription</code>
+    *         objects.
+    */
    public PropertyDescription[] getPropertiesDescriptions() {
       PropertyDescription[] pds = new PropertyDescription[3];
       pds[0] =
@@ -256,47 +325,44 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
                                  "time it is fired.");
 
       return pds;
-   }
+   } // end method getPropertiesDescriptions
 
    /**
-    * The total times the module will fire.
+    * Returns the total times the module will fire.
     *
-    * @return Description of return value.
+    * @return Total times the module will fire.
     */
    public int getTimesToFire() { return N; }
 
+   /**
+    * Returns whether or not to use the property 'N'.
+    *
+    * @return Whether or not to use the property 'N'.
+    */
    public boolean getUsePropNValue() { return usePropNValue; }
 
-   //////////////////////////
-   ///d2k control methods
-   ///////////////////////
-
    /**
-    * isReady() At first, checks to see if the object is in and if 
-    * it should
-    * wait for input (1) (the Integer N), otherwise returns the 
-    * superclass's
-    * isReady. Once it has the object and knows how many times to 
-    * pass it,
-    * returns true until it has been triggered all N times.
+    * Called by the D2K Infrastructure to determine if the module is ready to
+    * run. In this case, checks to see if the object is in and if it should wait
+    * for input (1) (the Integer N), otherwise returns the superclass's isReady.
+    * Once it has the object and knows how many times to pass it, returns true
+    * until it has been triggered all N times.
     *
-    * @return isReady() At first, checks to see if the object is in 
-    * and if it
-    *         should wait for input (1) (the Integer N), otherwise 
-    *         returns the
-    *         superclass's isReady. Once it has the object and knows 
-    *         how many
-    *         times to pass it, returns true until it has been 
-    *         triggered all N
-    *         times.
+    * @return Whether or not the module is ready to run.
     */
    public boolean isReady() {
 
       if (numFires == 0) { // this is the 'first' run
 
          if (
-             (this.getFlags()[0] > 0) && // we have the object to push
-                ((usePropNValue) || (this.getFlags()[1] > 0))) { // we have the number of times
+             
+
+                // we have the object to push
+                (this.getFlags()[0] > 0) &&
+                
+
+                // we have the number of times
+                ((usePropNValue) || (this.getFlags()[1] > 0))) {
             return true;
          } else {
             return false;
@@ -311,14 +377,26 @@ public class MultiPusher extends ncsa.d2k.core.modules.DataPrepModule
       return false;
    }
 
+   /**
+    * Sets the debug value.
+    *
+    * @param b Whether or not debug should be enabled.
+    */
    public void setDebug(boolean b) { debug = b; }
 
    /**
-    * Set the total times to fire (default = 4).
+    * Sets the total times to fire (default = 4).
     *
-    * @param i Description of parameter i.
+    * @param i The total number of times to fire.
     */
    public void setTimesToFire(int i) { N = i; }
 
+   /**
+    * Specifies whether or not to use the property 'N' when determining
+    * how many times to push the module.
+    *
+    * @param b Whether or not to use the N property when determining how many
+    * times to push the module
+    */
    public void setUsePropNValue(boolean b) { usePropNValue = b; }
 } // end class MultiPusher
