876ceeb3720214c5894c4278c15774cdb47f00c9
Robert McGrath <mcgrath@illinois.edu>
2006-07-26 14:28:29 +0000

    Format and comment clean up.

--- basic/src/ncsa/d2k/modules/core/datatype/Node.java
+++ basic/src/ncsa/d2k/modules/core/datatype/Node.java
@@ -1,169 +1,241 @@
+/*
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ *
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.datatype;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeMap;
 
 
 /**
-   Node is an implementation of an ADTree node. It stores a count and a list
-   of varNodes
-**/
+ * Node is an implementation of an ADTree node. It stores a count and a list of
+ * varNodes.
+ *
+ * @author  $Author$
+ * @version $Revision$, $Date$
+ * @see     ncsa.d2k.modules.core.datatype.ADTree
+ */
 
 public class Node {
 
-    // key - attribute index
-    // value - HashMap ( key attribute value, value Node) 
-    public int count;
-    public TreeMap varNodes;
+   //~ Instance fields *********************************************************
 
-    /**
-     Create a new emtpy Node
-    */    
-    
-    public Node( ) {
-	count = 0;
-	varNodes = null; 
-    }
-	
-    /**
-     Create a new Node with the specified count and list of varNodes
-     @param cnt the initial count
-     @param vNodes list of varNodes
+   /**
+    * key - attribute index value - HashMap ( key attribute value, value Node).
     */
-    public Node(int cnt, TreeMap vNodes){
-	count = cnt;
-	if( vNodes != null) 
-	    varNodes = new TreeMap(vNodes);
-    }
-	
+   public int count;
 
-	
-    public boolean containsKey(Object key) {
-	if (varNodes == null)
-	    return false;
-	else
-	    return varNodes.containsKey(key);
-    }
+   public TreeMap varNodes;
 
-	
-    public void putVarNode(int index, HashMap node) {
-	if (varNodes == null )
-	    varNodes = new TreeMap();
-	varNodes.put(new Integer(index), node);
-    }
-    
-    public HashMap getVarNode(int index) {
-	if (varNodes == null)
-	    return null;
-	else
-	    return (HashMap)varNodes.get(new Integer(index));
-    }
-    
-    public TreeMap getVarNodes() {
-	return varNodes;
-    }
+   //~ Constructors ************************************************************
 
-    
-    /*
-      add node nd to this node - add all branches too 
-      NOTE: add the counts of the node before or after calling this
-            function which does not add these counts.
-       @param nd node to be added to this node
+   /**
+    * Create a new emtpy Node.
     */
-      
-    public void addNode(Node nd){
-	TreeMap vNodes = nd.getVarNodes();
 
-	//System.out.println(" adding node nd1 " + this);
-	//System.out.println("to node nd2 " + nd);
-	
-	if (vNodes != null) {
-	    Iterator keys = vNodes.keySet().iterator();
-	    Integer key;
-	    while(keys.hasNext()) {
-		key = ((Integer)keys.next());
-		HashMap hm = new HashMap();
-		if(varNodes == null ) varNodes = new TreeMap();
-		hm = addHashMaps(hm,  (HashMap)vNodes.get(key));
-		hm = addHashMaps(hm, (HashMap)varNodes.get(key));
-		varNodes.put(key, hm);
-	    }
-	}
-	//	    return this;
-	//System.out.println("result node " + this);
-    }
-    
+   public Node() {
+      count = 0;
+      varNodes = null;
+   }
 
-    
-    /**
-       add second HashMap to first and returns first 
-       @param first HashMap  to be summed
-       @param second HashMap  to be summed
-       @return HashMap containing the sum of all nodes contained in the maps
+   /**
+    * Create a new Node with the specified count and list of varNodes.
+    *
+    * @param cnt    the initial count
+    * @param vNodes list of varNodes
     */
-    public HashMap addHashMaps(HashMap first, HashMap second) {
-	
-	if( first == null)
-	    return second;
+   public Node(int cnt, TreeMap vNodes) {
+      count = cnt;
 
-	if(second == null)
-	    return first;
+      if (vNodes != null) {
+         varNodes = new TreeMap(vNodes);
+      }
+   }
 
-	Iterator keys = second.keySet().iterator();
-	Node nd; 
-	while(keys.hasNext()) {
-	    String key = (String) keys.next();
-	    //System.out.println("KEY " + key);
-	    //System.out.println("1.first " + first);
-	    if (first.containsKey(key)) {
-		nd = (Node) first.get(key);
-		//System.out.println("nd1 " + nd);
-		nd.count = nd.count + ((Node)second.get(key)).count;
-		nd.addNode((Node)second.get(key));
-		//System.out.println("nd2 " + nd);
+   //~ Methods *****************************************************************
 
-	    }    
-	    else {
-		//System.out.println("second " + second);
-		nd = new Node(((Node)second.get(key)).count,
-			      ((Node)second.get(key)).getVarNodes());
-		//System.out.println("nd3 " + nd);
-		first.put(key,nd);
-	    }
-	}
-	
-	//	System.out.println("returned from addHashMap " + first);
-	return first;
-    }
+   /**
+    * add second HashMap to first and returns first.
+    *
+    * @param  first  HashMap to be summed
+    * @param  second HashMap to be summed
+    *
+    * @return HashMap containing the sum of all nodes contained in the maps.
+    */
+   public HashMap addHashMaps(HashMap first, HashMap second) {
 
+      if (first == null) {
+         return second;
+      }
 
+      if (second == null) {
+         return first;
+      }
 
-    public int getCount() {
-	return count;
-    }
+      Iterator keys = second.keySet().iterator();
+      Node nd;
 
+      while (keys.hasNext()) {
+         String key = (String) keys.next();
 
-    public String toString() {
-	String result = "";
-	if( count != 0 ) result = "node count " + count + " ";
-	if (varNodes == null)  return result;
-	Iterator keys = varNodes.keySet().iterator();
-	Integer key;
-	String indent ="";
-	while(keys.hasNext()) {
-	    key = (Integer) keys.next();
-	    for (int i = 0; i < key.intValue() ; i ++)
-		indent = indent + " " ;
+         if (first.containsKey(key)) {
+            nd = (Node) first.get(key);
 
-	    result = result + "varN " + key + " has HM with keys \n" + indent 
-			       + ((HashMap)varNodes.get(key));
-	    
-	    indent = ""; 
-	    for (int i = 0; i < key.intValue()-2 ; i ++)
-		indent = indent + " " ;
-	    result = result + "\n" + indent; //+ "- ";
-	}
-	return result;
-    }
-    
-}
+            nd.count = nd.count + ((Node) second.get(key)).count;
+            nd.addNode((Node) second.get(key));
 
+         } else {
+
+            nd =
+               new Node(((Node) second.get(key)).count,
+                        ((Node) second.get(key)).getVarNodes());
+
+            first.put(key, nd);
+         }
+      }
+
+      return first;
+   } // end method addHashMaps
+
+   /**
+    * add node nd to this node - add all branches too NOTE: add the counts of
+    * the node before or after calling this function which does not add these
+    * counts.
+    *
+    * @param nd node to be added to this node
+    */
+   public void addNode(Node nd) {
+      TreeMap vNodes = nd.getVarNodes();
+
+      if (vNodes != null) {
+         Iterator keys = vNodes.keySet().iterator();
+         Integer key;
+
+         while (keys.hasNext()) {
+            key = ((Integer) keys.next());
+
+            HashMap hm = new HashMap();
+
+            if (varNodes == null) {
+               varNodes = new TreeMap();
+            }
+
+            hm = addHashMaps(hm, (HashMap) vNodes.get(key));
+            hm = addHashMaps(hm, (HashMap) varNodes.get(key));
+            varNodes.put(key, hm);
+         }
+      }
+   } // end method addNode
+
+   public boolean containsKey(Object key) {
+
+      if (varNodes == null) {
+         return false;
+      } else {
+         return varNodes.containsKey(key);
+      }
+   }
+   
+   public int getCount() { return count; }
+
+   public HashMap getVarNode(int index) {
+
+      if (varNodes == null) {
+         return null;
+      } else {
+         return (HashMap) varNodes.get(new Integer(index));
+      }
+   }
+
+   public TreeMap getVarNodes() { return varNodes; }
+
+   public void putVarNode(int index, HashMap node) {
+
+      if (varNodes == null) {
+         varNodes = new TreeMap();
+      }
+
+      varNodes.put(new Integer(index), node);
+   }
+
+   public String toString() {
+      String result = "";
+
+      if (count != 0) {
+         result = "node count " + count + " ";
+      }
+
+      if (varNodes == null) {
+         return result;
+      }
+
+      Iterator keys = varNodes.keySet().iterator();
+      Integer key;
+      String indent = "";
+
+      while (keys.hasNext()) {
+         key = (Integer) keys.next();
+
+         for (int i = 0; i < key.intValue(); i++) {
+            indent = indent + " ";
+         }
+
+         result =
+            result + "varN " + key + " has HM with keys \n" + indent +
+            ((HashMap) varNodes.get(key));
+
+         indent = "";
+
+         for (int i = 0; i < key.intValue() - 2; i++) {
+            indent = indent + " ";
+         }
+
+         result = result + "\n" + indent; // + "- ";
+      }
+
+      return result;
+   } // end method toString
+
+} // end class Node
--- basic/src/ncsa/d2k/modules/core/datatype/graph/GraphUtils.java
+++ basic/src/ncsa/d2k/modules/core/datatype/graph/GraphUtils.java
@@ -1,29 +1,76 @@
+/* 
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ * 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ * 
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.datatype.graph;
 
 /**
  * A collection of graph-related constants and utilities.
+ *
+ * @author  $Author$
+ * @version $Revision$, $Date$
  */
 public class GraphUtils {
 
-  /**
-   * The recommended key for "label" attributes.
-   */
-  public static final String LABEL = "label";
+   //~ Static fields/initializers **********************************************
 
-  /**
-   * The recommended key for an attribute that places a vertex into a
-   * partition of a k-partite graph (e.g., a bipartite graph).
-   */
-  public static final String PARTITION = "partition";
+   /** The recommended key for "label" attributes. */
+   static public final String LABEL = "label";
 
-  /**
-   * The recommended key for "weight" attributes.
-   */
-  public static final String WEIGHT = "weight";
+   /**
+    * The recommended key for an attribute that places a vertex into a partition
+    * of a k-partite graph (e.g., a bipartite graph).
+    */
+   static public final String PARTITION = "partition";
 
-  /**
-   * This class is not intended to be instantiated.
-   */
-  private GraphUtils() { }
+   /** The recommended key for "weight" attributes. */
+   static public final String WEIGHT = "weight";
+
+   //~ Constructors ************************************************************
+
+   /**
+    * This class is not intended to be instantiated.
+    */
+   private GraphUtils() { }
 
 }
