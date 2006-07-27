0bb317ed114dd196c00008e7f0a05c0dae55a89a
Robert McGrath <mcgrath@illinois.edu>
2006-07-27 17:30:05 +0000

    Format and comment clean up.

--- basic/src/ncsa/d2k/modules/core/util/AddResource.java
+++ basic/src/ncsa/d2k/modules/core/util/AddResource.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * $Header$
  *
  * ===================================================================
@@ -6,17 +6,17 @@
  * D2K-Workflow
  * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
  * ILLINOIS. All rights reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License v2.0
  * as published by the Free Software Foundation and with the required
  * interpretation regarding derivative works as described below.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License v2.0 for more details.
- * 
+ *
  * This program and the accompanying materials are made available
  * under the terms of the GNU General Public License v2.0 (GPL v2.0)
  * which accompanies this distribution and is available at
@@ -34,7 +34,7 @@
  * make those components a derivative work of D2K-Workflow.
  * (Examples of such independently developed components include for
  * example, external databases or metadata and provenance stores).
- * 
+ *
  * Note: A non-GPL commercially licensed version of contributions
  * from the UNIVERSITY OF ILLINOIS may be available from the
  * designated commercial licensee RiverGlass, Inc. located at
@@ -95,7 +95,14 @@ public abstract class AddResource extends DataPrepModule {
       }
    }
 
-
+   /**
+    * Returns a description of the input at the specified index.
+    *
+    * @param  index Index of the input for which a description should be
+    *               returned.
+    *
+    * @return <code>String</code> describing the input at the specified index.
+    */
    public String getInputInfo(int index) {
 
       switch (index) {
@@ -105,6 +112,14 @@ public abstract class AddResource extends DataPrepModule {
       }
    }
 
+   /**
+    * Returns the name of the input at the specified index.
+    *
+    * @param  index Index of the input for which a name should be returned.
+    *
+    * @return <code>String</code> containing the name of the input at the
+    *         specified index.
+    */
    public String getInputName(int index) {
 
       switch (index) {
@@ -114,12 +129,24 @@ public abstract class AddResource extends DataPrepModule {
       }
    }
 
+   /**
+    * Returns an array of <code>String</code> objects each containing the fully
+    * qualified Java data type of the input at the corresponding index.
+    *
+    * @return An array of <code>String</code> objects each containing the fully
+    *         qualified Java data type of the input at the corresponding index.
+    */
    public String[] getInputTypes() {
       String[] types = {};
 
       return types;
    }
 
+   /**
+    * Describes the purpose of the module.
+    *
+    * @return <code>String</code> describing the purpose of the module.
+    */
    public String getModuleInfo() {
       return "This module adds an object as a resource, " +
              "it registers the object as a resource with the Resource " +
@@ -127,8 +154,22 @@ public abstract class AddResource extends DataPrepModule {
              "by the String <i>Resource ID</i>.";
    }
 
+   /**
+    * Returns the name of the module that is appropriate for end user
+    * consumption.
+    *
+    * @return The name of the module.
+    */
    public String getModuleName() { return "Add Resource Module"; }
 
+   /**
+    * Returns a description of the output at the specified index.
+    *
+    * @param  index Index of the output for which a description should be
+    *               returned.
+    *
+    * @return <code>String</code> describing the output at the specified index.
+    */
    public String getOutputInfo(int index) {
 
       switch (index) {
@@ -141,6 +182,15 @@ public abstract class AddResource extends DataPrepModule {
       }
    }
 
+   /**
+    * Returns the name of the output at the specified index.
+    *
+    * @param  index Index of the output for which a description should be
+    *               returned.
+    *
+    * @return <code>String</code> containing the name of the output at the
+    *         specified index.
+    */
    public String getOutputName(int index) {
 
       switch (index) {
@@ -153,26 +203,45 @@ public abstract class AddResource extends DataPrepModule {
       }
    }
 
+   /**
+    * Returns an array of <code>String</code> objects each containing the fully
+    * qualified Java data type of the output at the corresponding index.
+    *
+    * @return An array of <code>String</code> objects each containing the fully
+    *         qualified Java data type of the output at the corresponding index.
+    */
    public String[] getOutputTypes() {
       String[] types = { "java.lang.Object" };
 
       return types;
    }
 
+   /**
+    * Returns an array of <code>ncsa.d2k.core.modules.PropertyDescription</code>
+    * objects for each property of the module.
+    *
+    * @return An array of <code>ncsa.d2k.core.modules.PropertyDescription</code>
+    *         objects.
+    */
    public PropertyDescription[] getPropertiesDescriptions() {
       PropertyDescription[] pds = new PropertyDescription[1];
       pds[0] =
          new PropertyDescription("rscID", "Resource ID",
-            "The resource ID for the object prepared/loaded by this module.");
+                                 "The resource ID for the object prepared/loaded by this module.");
 
       return pds;
    }
 
+   /**
+    * Description of method getRscID.
+    *
+    * @return Description of return value.
+    */
    public String getRscID() { return rscID; }
-   
+
    /**
     * Set the resource ID for the resource manager to use.
-    * 
+    *
     * @param id the resource id.
     */
    public void setRscID(String id) { rscID = id; }
