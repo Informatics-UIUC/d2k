14b6c441f636894c31b62eb6a7f2a28d713f5dac
Andrew Shirk <shirk@illinois.edu>
2006-07-31 21:33:12 +0000

    Completed documentation improvements for open source

--- basic/src/ncsa/d2k/modules/core/datatype/table/Row.java
+++ basic/src/ncsa/d2k/modules/core/datatype/table/Row.java
@@ -1,100 +1,180 @@
+/*
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ *
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.datatype.table;
 
+/**
+ * Interface for a <code>Table</code> row.
+ *
+ * @author  suvalala
+ * @version $Revision$, $Date$
+ */
 public interface Row {
 
-	/**
-	 * set the index of the row to access.
-	 * @param i the index of the row to access.
-	 */
-	public void setIndex(int i);
+   //~ Methods *****************************************************************
 
-	/**
-	 * Get the ith input as a double.
-	 * @param i the input index
-	 * @return the ith input as a double
-	 */
-	public double getDouble(int i);
+   /**
+    * Gets the ith input as a <code>boolean</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>boolean</code>
+    */
+   public boolean getBoolean(int i);
 
-	/**
-	 * Get the ith input as a String.
-	 * @param i the input index
-	 * @return the ith input as a String
-	 */
-	public String getString(int i);
+   /**
+    * Gets the ith input as a <code>byte</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>byte</code>
+    */
+   public byte getByte(int i);
 
-	/**
-	 * Get the ith input as an int.
-	 * @param i the input index
-	 * @return the ith input as an int
-	 */
-	public int getInt(int i);
+   /**
+    * Gets the ith input as a <code>byte</code> array.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>byte</code> array
+    */
+   public byte[] getBytes(int i);
 
-	/**
-	 * Get the ith input as a float.
-	 * @param i the input index
-	 * @return the ith input as a float
-	 */
-	public float getFloat(int i);
+   /**
+    * Gets the ith input as a <code>char</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>char</code>
+    */
+   public char getChar(int i);
 
-	/**
-	 * Get the ith input as a short.
-	 * @param i the input index
-	 * @return the ith input as a short
-	 */
-	public short getShort(int i);
+   /**
+    * Gets the ith input as a <code>char</code> array.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>char</code> array
+    */
+   public char[] getChars(int i);
 
-	/**
-	 * Get the ith input as a long.
-	 * @param i the input index
-	 * @return the ith input as a long
-	 */
-	public long getLong(int i);
+   /**
+    * Gets the ith input as a <code>double</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>double</code>
+    */
+   public double getDouble(int i);
 
-	/**
-	 * Get the ith input as a byte.
-	 * @param i the input index
-	 * @return the ith input as a byte
-	 */
-	public byte getByte(int i);
+   /**
+    * Gets the ith input as a <code>float</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>float</code>
+    */
+   public float getFloat(int i);
 
-	/**
-	 * Get the ith input as an Object.
-	 * @param i the input index
-	 * @return the ith input as an Object.
-	 */
-	public Object getObject(int i);
+   /**
+    * Gets the ith input as an <code>int</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as an <code>int</code>
+    */
+   public int getInt(int i);
 
-	/**
-	 * Get the ith input as a char.
-	 * @param i the input index
-	 * @return the ith input as a char
-	 */
-	public char getChar(int i);
+   /**
+    * Gets the ith input as a <code>long</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>long</code>
+    */
+   public long getLong(int i);
 
-	/**
-	 * Get the ith input as chars.
-	 * @param i the input index
-	 * @return the ith input as chars
-	 */
-	public char[] getChars(int i);
+   /**
+    * Gets the ith input as an <code>Object</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as an <code>Object</code>
+    */
+   public Object getObject(int i);
 
-	/**
-	 * Get the ith input as bytes.
-	 * @param i the input index
-	 * @return the ith input as bytes.
-	 */
-	public byte[] getBytes(int i);
+   /**
+    * Gets the ith input as a <code>short.</code>
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>short</code>
+    */
+   public short getShort(int i);
 
-	/**
-	 * Get the ith input as a boolean.
-	 * @param i the input index
-	 * @return the ith input as a boolean
-	 */
-	public boolean getBoolean(int i);
+   /**
+    * Gets the ith input as a <code>String</code>.
+    *
+    * @param  i The input index
+    *
+    * @return The ith input as a <code>String</code>
+    */
+   public String getString(int i);
 
-	/**
-	 * Get a reference to the table this example is part of.
-	 * @return a reference to the table.
-	 */
-	public Table getTable();
-}
+   /**
+    * Gets a reference to the <code>Table</code> this <code>Row</code> is part
+    * of.
+    *
+    * @return Reference to the <code>Table</code> this <code>Row</code> is part
+    *         of
+    */
+   public Table getTable();
+
+   /**
+    * Sets the index of the row to access.
+    *
+    * @param i Index of the row to access
+    */
+   public void setIndex(int i);
+} // end interface Row
