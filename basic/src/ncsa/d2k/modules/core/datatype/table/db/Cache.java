e88ff503b1f5b6a6ea2b99cb5830ae81315ef82e
Andrew Shirk <shirk@illinois.edu>
2006-08-02 21:53:36 +0000

    Completed documentation improvements for open source

--- basic/src/ncsa/d2k/modules/core/datatype/table/db/Cache.java
+++ basic/src/ncsa/d2k/modules/core/datatype/table/db/Cache.java
@@ -1,119 +1,274 @@
+/*
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ *
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.datatype.table.db;
 
-import java.sql.*;
-import ncsa.d2k.modules.core.datatype.table.*;
-import ncsa.d2k.modules.core.datatype.table.basic.*;
+import ncsa.d2k.modules.core.datatype.table.ColumnTypes;
+import ncsa.d2k.modules.core.datatype.table.basic.MutableTableImpl;
+
+import java.sql.SQLException;
+
+
 /**
- * <p>Title: CacheMissException </p>
- * <p>Description: Abstract class implementing the common Cache functionality </p>
- * <p>Copyright: NCSA (c) 2002</p>
- * <p>Company: </p>
- * @author Sameer Mathur, David Clutter
- * @version 1.0
+ * Abstract class implementing the common cache functionality.
+ *
+ * @author  mathur
+ * @author  clutter
+ * @version $Revision$, $Date$
  */
 
 public abstract class Cache implements java.io.Serializable {
 
-    protected MutableTableImpl table;
+   //~ Static fields/initializers **********************************************
 
-    // range of row# from the entire ResultSet, that are stored in the Cache
-    protected int minRowNum; //(indicates the beginning of the ResultSet subset cached)
-    protected int maxRowNum; //(indicates the end of the ResultSet subset cached)
+   /** Use serialVersionUID for interoperability. */
+   static private final long serialVersionUID = -1798715195654906371L;
 
-    protected int numRowsInCache; // current # of rows in the Cache
-    protected int maxRowsInCache;  // maximum # of rows that can be in the Cache
-    protected int numRows;
+   //~ Instance fields *********************************************************
 
-    protected int getNumCacheRows() {
-        return numRowsInCache;
-    }
-    protected void setNumCacheRows(int cacheRows) {
-        numRowsInCache = cacheRows;
-    }
-    protected int getMaxCacheRows() {
-        return maxRowsInCache;
-    }
-    protected void setMaxCacheRows(int calculatedMaxNumRows) {
-        maxRowsInCache = calculatedMaxNumRows;
-    }
+   /** Indicates the end of the <code>ResultSet</code> subset cached. */
+   protected int maxRowNum;
+
+   /** Maximum number of rows that can be in the cache. */
+   protected int maxRowsInCache;
+
+   /** Indicates the beginning of the <code>ResultSet</code> subset cached. */
+   protected int minRowNum;
+
+   /** Current number of rows in the cache. */
+   protected int numRowsInCache;
+
+   /** Stores the cached data. */
+   protected MutableTableImpl table;
+
+   //~ Methods *****************************************************************
+
+   /**
+    * Returns the local row number in the cache where the data is to be found.
+    * Returns -1 if the data is not in the cache.
+    *
+    * @param  row Row to search for
+    *
+    * @return Row number in the cache where the row is found
+    */
+   private int whichRowInCache(int row) {
+
+      try {
+
+         if ((row >= minRowNum) && (row <= maxRowNum)) { // found row in Cache
+            return (row - minRowNum); // return the row
+         } else {
+            return -1; // couldn't find row in Cache
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+
+         return -2;
+      }
+   }
+
+   /**
+    * Returns the maximum number of rows that can be in the cache.
+    *
+    * @return Max number of rows that can be in the cache
+    */
+   protected int getMaxCacheRows() { return maxRowsInCache; }
+
+   /**
+    * Returns the current number of rows that are cached.
+    *
+    * @return Current number of rows that are cached
+    */
+   protected int getNumCacheRows() { return numRowsInCache; }
+
+   /**
+    * Sets the maximum number of rows to cache.
+    *
+    * @param calculatedMaxNumRows Max number of rows to cache
+    */
+   protected void setMaxCacheRows(int calculatedMaxNumRows) {
+      maxRowsInCache = calculatedMaxNumRows;
+   }
+
+   /**
+    * Sets the number of rows that are in the cache.
+    *
+    * @param cacheRows Number of rows that are in the cache
+    */
+   protected void setNumCacheRows(int cacheRows) { numRowsInCache = cacheRows; }
+
+   /**
+    * Returns the data at the specified row and column as a boolean value.
+    *
+    * @param  row Row to find the data
+    * @param  col Column to find the data
+    *
+    * @return Boolean value of the data
+    *
+    * @throws CacheMissException If the specified row is not in the cache
+    */
+   public boolean getCacheBooleanData(int row, int col)
+      throws CacheMissException {
+      int rowInCache = whichRowInCache(row);
+
+      if (rowInCache != -1) {
+         return table.getBoolean(rowInCache, col);
+      } else {
+         throw new CacheMissException("Cache:GetCacheBooleanData:CacheMissException");
+      }
+   }
+
+   /**
+    * Returns the data at the specified row and column as a double value.
+    *
+    * @param  row Row to find the data
+    * @param  col Column to find the data
+    *
+    * @return Double value of the data
+    *
+    * @throws CacheMissException If the specified row is not in the cache
+    */
+   public double getCacheNumericData(int row, int col)
+      throws CacheMissException {
+      int rowInCache = whichRowInCache(row);
+
+      if (rowInCache != -1) {
+         return table.getDouble(rowInCache, col);
+      } else {
+         throw new CacheMissException("Cache:GetCacheNumericData:CacheMissException");
+      }
+   }
+
+   /**
+    * Returns the data at the specified row and column as an <code>
+    * Object</code>.
+    *
+    * @param  row Row to find the data
+    * @param  col Column to find the data
+    *
+    * @return <code>Object</code> value of the data
+    *
+    * @throws CacheMissException If the specified row is not in the cache
+    */
+   public Object getCacheObjectData(int row, int col)
+      throws CacheMissException {
+      int rowInCache = whichRowInCache(row);
+
+      if (rowInCache != -1) {
+         return table.getObject(rowInCache, col);
+      } else {
+         throw new CacheMissException("Cache:getCacheObjectData:CacheMissException");
+      }
+   }
 
 
-    public String getCacheTextData(int row, int col) throws CacheMissException {
-        int rowInCache = whichRowInCache(row);
-        if (rowInCache != -1)
-            return table.getString(rowInCache,col);
-        else
-            throw new CacheMissException("Cache:getCacheTextData:CacheMissException");
-    } //getCacheTextData
+   /**
+    * Returns the data at the specified row and column as a String value.
+    *
+    * @param  row Row to find the data
+    * @param  col Column to find the data
+    *
+    * @return String value of the data
+    *
+    * @throws CacheMissException If the specified row is not in the cache
+    */
+   public String getCacheTextData(int row, int col) throws CacheMissException {
+      int rowInCache = whichRowInCache(row);
 
-    public double getCacheNumericData(int row, int col) throws CacheMissException {
-        int rowInCache = whichRowInCache(row);
-        if (rowInCache != -1)
-            return table.getDouble(rowInCache,col);
-        else
-            throw new CacheMissException("Cache:GetCacheNumericData:CacheMissException");
-    } //getCacheNumericData
+      if (rowInCache != -1) {
+         return table.getString(rowInCache, col);
+      } else {
+         throw new CacheMissException("Cache:getCacheTextData:CacheMissException");
+      }
+   }
 
-    public boolean getCacheBooleanData(int row, int col) throws CacheMissException {
-        int rowInCache = whichRowInCache(row);
-        if (rowInCache != -1)
-            return table.getBoolean(rowInCache,col);
-        else
-            throw new CacheMissException("Cache:GetCacheBooleanData:CacheMissException");
-    } //getCacheBooleanData
+   /**
+    * Updates the cache.
+    *
+    * @param  rowInResultSet Uses as the minimum row number to cache
+    * @param  dbds           Datasource to use when populating cache
+    *
+    * @throws SQLException If a problem occurs while using the datasource
+    */
+   public void update(int rowInResultSet, DBDataSource dbds)
+      throws SQLException {
+      minRowNum = rowInResultSet;
 
-    public Object getCacheObjectData(int row, int col) throws CacheMissException {
-        int rowInCache = whichRowInCache(row);
-        if (rowInCache != -1)
-            return table.getObject(rowInCache,col);
-        else
-            throw new CacheMissException("Cache:getCacheObjectData:CacheMissException");
-    } //getCacheObjectData
+      int r = rowInResultSet;
 
+      while (r < getMaxCacheRows() && r < dbds.getNumRows()) {
 
-    // return the physical row#  in the Cache, where the data is to be found.
-    // return -1 if the data is not in the Cache
-    private int whichRowInCache (int row) {
-        try {
-            if ((row >= minRowNum) && (row <= maxRowNum)) //found row in Cache
-                return (row-minRowNum); // return the row
-            else
-                return -1; //couldn't find row in Cache
-        }
-        catch (Exception e){
-            e.printStackTrace();
-           return -2;
-        }
-    }// RowInCache
+         for (int c = 0; c < dbds.getNumDistinctColumns(); c++) {
 
-    public void update(int rowInResultSet, DBDataSource dbds) throws SQLException {
-        minRowNum = rowInResultSet;
+            switch (dbds.getColumnType(c)) {
 
-        int r = rowInResultSet;
-        while ( r < getMaxCacheRows() && r < dbds.getNumRows()) {
-            for (int c = 0; c < dbds.getNumDistinctColumns(); c++) {
-                switch (dbds.getColumnType(c)) {
-                    case ColumnTypes.INTEGER:
-                    case ColumnTypes.DOUBLE:
-                    case ColumnTypes.FLOAT:
-                        table.setDouble(dbds.getNumericData(r,c), r, c);
-                        break;
-                    case ColumnTypes.STRING:
-                    case ColumnTypes.CHAR:
-                    case ColumnTypes.OBJECT:
-                        table.setString(dbds.getTextData(r,c), r, c);
-                        break;
-                    default:
-                        table.setString(dbds.getTextData(r,c), r, c);
-                } //switch
-            }//for loop processes all the columns for a particular row
-            r++;
-        }//while
+               case ColumnTypes.INTEGER:
+               case ColumnTypes.DOUBLE:
+               case ColumnTypes.FLOAT:
+                  table.setDouble(dbds.getNumericData(r, c), r, c);
 
-        //now update how many rows were written to the cache
-        setNumCacheRows(r);
-        //now update maxRowNum
-        maxRowNum = minRowNum + getNumCacheRows() -1;
-    }
+                  break;
 
-} //Cache
+               case ColumnTypes.STRING:
+               case ColumnTypes.CHAR:
+               case ColumnTypes.OBJECT:
+                  table.setString(dbds.getTextData(r, c), r, c);
+
+                  break;
+
+               default:
+                  table.setString(dbds.getTextData(r, c), r, c);
+            }
+         } // for loop processes all the columns for a particular row
+
+         r++;
+      } // end while
+
+      // now update how many rows were written to the cache
+      setNumCacheRows(r);
+
+      maxRowNum = minRowNum + getNumCacheRows() - 1;
+   } // end method update
+
+} // end class Cache
--- basic/src/ncsa/d2k/modules/core/datatype/table/db/CacheMissException.java
+++ basic/src/ncsa/d2k/modules/core/datatype/table/db/CacheMissException.java
@@ -1,22 +1,78 @@
+/*
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ *
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.datatype.table.db;
 
 /**
- * <p>Title: CacheMissException </p>
- * <p>Description: Detects when a cache miss occurs </p>
- * <p>Copyright: NCSA (c) 2002</p>
- * <p>Company: </p>
- * @author Sameer Mathur, David Clutter
- * @version 1.0
+ * Signals that a cache miss has occurred.
+ *
+ * @author  mathur
+ * @author  clutter
+ * @version $Revision$, $Date$
  */
 
 public class CacheMissException extends Exception {
 
-  public CacheMissException() {
-      super("A Cache Miss has occured");
-  }
+   //~ Static fields/initializers **********************************************
 
-  public CacheMissException(String message) {
-      super(message);
-  }
+   /** Use serialVersionUID for interoperability. */
+   static private final long serialVersionUID = -4012893484743366490L;
+
+   //~ Constructors ************************************************************
+
+   /**
+    * Creates a new <code>CacheMissException</code> object with the default
+    * message.
+    */
+   public CacheMissException() { super("A Cache Miss has occured"); }
+
+   /**
+    * Creates a new <code>CacheMissException</code> object with the supplied
+    * message.
+    *
+    * @param message Message to supply to the superclass.
+    */
+   public CacheMissException(String message) { super(message); }
 
 }
