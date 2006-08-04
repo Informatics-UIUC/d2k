e6149c7d8653934533523fa3f8d6dd92c6744c3c
Andrew Shirk <shirk@illinois.edu>
2006-08-04 20:15:45 +0000

    With my new lowered standards, completed documentation improvements for open source

--- basic/src/ncsa/d2k/modules/core/datatype/table/NumericColumn.java
+++ basic/src/ncsa/d2k/modules/core/datatype/table/NumericColumn.java
@@ -1,41 +1,78 @@
+/* 
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ * 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ * 
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.datatype.table;
-import ncsa.d2k.modules.core.datatype.table.Column;
 
 
 /**
-	NumericColumn is an extension of Column which is implemented by all
-	classes which are optimized to represent and access numeric data.
-	<br>
-*/
+ * NumericColumn is an extension of Column which is implemented by all classes
+ * which are optimized to represent and access numeric data.
+ *
+ * @author  clutter
+ * @version $Revision$, $Date$
+ */
 public interface NumericColumn extends Column {
 
-	//static final long serialVersionUID = -8231597566891195839L;
-	static final long serialVersionUID = 1L;
+   //~ Static fields/initializers **********************************************
 
-    //////////////////////////////////////
-    //// Accessing Metadata
-    /**
-       Get the minimum value contained in this list
-       @return the minimum value of this list
+   /** The universal version identifier. */
+   static final long serialVersionUID = 1L;
+
+   //~ Methods *****************************************************************
+
+   /**
+    * Get the maximum value contained in this list.
+    *
+    * @return the maximum value of this list
     */
-    public double getMin();
+   public double getMax();
 
-    /**
-       Get the maximum value contained in this list
-       @return the maximum value of this list
+   /**
+    * Get the minimum value contained in this list.
+    *
+    * @return the minimum value of this list
     */
-    public double getMax();
+   public double getMin();
 
-    /**
-       Sets the value which indicates an empty entry.
-       @param emptyVal the value to which an empty entry is set
-    /
-    public void setEmptyValue( Number emptyVal );
-
-    /**
-       Gets the value which indicates an empty entry.
-       @return the value of an empty entry as a subclass of Number
-    /
-    public Number getEmptyValue( );
-	*/
-}/*NumericColumn*/
+}
