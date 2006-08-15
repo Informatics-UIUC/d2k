3b8cc75ef2b40ecbd42e5127ad23025242f9cf19
Andrew Shirk <shirk@illinois.edu>
2006-08-15 15:51:31 +0000

    Completed documentation improvements for open source

--- basic/src/ncsa/d2k/modules/core/datatype/table/sparse/primitivehash/ValueAdjuster.java
+++ basic/src/ncsa/d2k/modules/core/datatype/table/sparse/primitivehash/ValueAdjuster.java
@@ -1,35 +1,110 @@
+/* 
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ * 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ * 
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.datatype.table.sparse.primitivehash;
 
-import gnu.trove.*;
+import gnu.trove.TIntIntHashMap;
+import gnu.trove.TIntProcedure;
+
 
 /**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2004</p>
- * <p>Company: </p>
- * @author vered goren
- * @version 1.0
+ * For each key in <code>map</code>, if the its mapped value is greater than
+ * <code>value</code>, adjusts this value by <code>delta</code>.
  *
- * for each key in <code>map</code>, if the its mapped value is greater
- * than <code>value</code>, adjust this value by <codE>delta</code>.
+ * @author  goren
+ * @version $Revision$, $Date$
  */
 
 public class ValueAdjuster implements TIntProcedure {
-  public ValueAdjuster() {
-  }
-  TIntIntHashMap map;
-  int delta;
-  int value;
-  public ValueAdjuster(TIntIntHashMap _map, int _delta, int _value) {
-    map = _map;
-    delta = _delta;
-    value = _value;
-  }
-  public boolean execute(int key) {
-    if(map.get(key) > value)
-      map.adjustValue(key, delta);
 
-    return true;
-  }
+   //~ Instance fields *********************************************************
 
-}
+   /** Adjust values using this delta. */
+   int delta;
+
+   /** Map containing values to adjust. */
+   TIntIntHashMap map;
+
+   /** Value for comparison with those in the map. */
+   int value;
+
+   //~ Constructors ************************************************************
+
+   /**
+    * Creates a new ValueAdjuster object.
+    */
+   public ValueAdjuster() { }
+
+   /**
+    * Creates a new ValueAdjuster object.
+    *
+    * @param _map   Map to use
+    * @param _delta Delta to use
+    * @param _value Value to use
+    */
+   public ValueAdjuster(TIntIntHashMap _map, int _delta, int _value) {
+      map = _map;
+      delta = _delta;
+      value = _value;
+   }
+
+   //~ Methods *****************************************************************
+
+   /**
+    * Adjusts the value with the given key.
+    *
+    * @param  key Key with value to adjust
+    *
+    * @return Success/failure flag
+    */
+   public boolean execute(int key) {
+
+      if (map.get(key) > value) {
+         map.adjustValue(key, delta);
+      }
+
+      return true;
+   }
+
+} // end class ValueAdjuster
