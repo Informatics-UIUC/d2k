19868bc0b4c3ea99f6b1899f2353d64341759ead
Vered Goren <vered@illinois.edu>
2006-08-15 20:39:36 +0000

    All changes towards open source release.

--- basic/src/ncsa/d2k/modules/core/discovery/ruleassociation/fpgrowth/FPPattern.java
+++ basic/src/ncsa/d2k/modules/core/discovery/ruleassociation/fpgrowth/FPPattern.java
@@ -1,86 +1,223 @@
+/*
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ *
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.discovery.ruleassociation.fpgrowth;
 
-import java.util.*;
+import gnu.trove.TIntHashSet;
+import gnu.trove.TIntIterator;
+import gnu.trove.TIntObjectHashMap;
 
-import gnu.trove.*;
 
-public class FPPattern implements java.io.Serializable{
+/**
+ * Description of class FPPattern.
+ *
+ * @author  $Author$
+ * @version $Revision$, $Date$
+ */
+public class FPPattern implements java.io.Serializable {
 
-//==============
-// Data Members
-//==============
+   //~ Static fields/initializers **********************************************
 
-  private int _support = 0;
-  private TIntHashSet _patternElts = new TIntHashSet();
+   /** Use serialVersionUID for interoperability. */
+   static private final long serialVersionUID = 3553517370011965398L;
 
-  private static TIntObjectHashMap _eltMap = new TIntObjectHashMap();
+   /**
+    * maps attribute index to attribute name. the keys are members of <code>
+    * _patternElts</code>.
+    */
+   static private TIntObjectHashMap _eltMap = new TIntObjectHashMap();
 
-  public static void clearElementMapping(){
-    _eltMap.clear();
-  }
+   //~ Instance fields *********************************************************
 
-  public static void addElementMapping(int k, String v){
-    _eltMap.put(k,v);
-  }
+   /** The features (column indices) that make this pattern, as a set. */
+   private TIntHashSet _patternElts = new TIntHashSet();
 
-  public static String getElementLabel(int i){
-    return (String)_eltMap.get(i);
-  }
+   /** The support (expressed in percentage) of this pattern. */
+   private int _support = 0;
 
-//================
-// Constructor(s)
-//================
-  public FPPattern() {
-  }
+   //~ Constructors ************************************************************
 
-  public FPPattern(int[] col, int supp){
-    _support = supp;
-    if (col != null){
-      _patternElts.addAll(col);
-    }
-  }
+   /**
+    * Creates a new FPPattern object.
+    */
+   public FPPattern() { }
 
-  public FPPattern(int col, int supp){
-    _support = supp;
+   /**
+    * Constructs an FPPAttern with the columns indices <code>col</codE> and the
+    * support level <code>supp.</code>
+    *
+    * @param col  int[] Columns indices that make this pattern
+    * @param supp int The support of this pattern.
+    */
+   public FPPattern(int[] col, int supp) {
+      _support = supp;
+
+      if (col != null) {
+         _patternElts.addAll(col);
+      }
+   }
+
+   /**
+    * Constructs an FPPattern with one column <codE>col</code> and the support
+    * level <codE>supp.</codE>
+    *
+    * @param col  int The index of the feature that makes this pattern
+    * @param supp int The support level of the pattern
+    */
+   public FPPattern(int col, int supp) {
+      _support = supp;
       _patternElts.add(col);
-  }
+   }
 
-//================
-// Public Methods
-//================
+   //~ Methods *****************************************************************
 
-  public FPPattern copy(){
-    FPPattern newpat = new FPPattern();
-    newpat._support = this._support;
-    newpat._patternElts.addAll(_patternElts.toArray());
-    return newpat;
-  }
+   /**
+    * Adds a mapping of index <codE>k</code> to attribute name <code>v<c/code>.</code>
+    *
+    * @param k int The index of the element named <code>v</codE>
+    * @param v String The name of attribute with index <codE>k</code>
+    */
+   static public void addElementMapping(int k, String v) { _eltMap.put(k, v); }
 
-  public int getSize(){
-    return _patternElts.size();
-  }
+   /**
+    * Clears the index to name map.
+    */
+   static public void clearElementMapping() { _eltMap.clear(); }
 
-  public TIntIterator getPattern(){
-    return _patternElts.iterator();
-  }
+   /**
+    * Returns the name of attribute index <code>i.</code>
+    *
+    * @param  i int The index of an attribute that is part of this pattern.
+    *
+    * @return String The name of attribute index <code>i</code>
+    */
+   static public String getElementLabel(int i) {
+      return (String) _eltMap.get(i);
+   }
 
-  public int getSupport(){
-    return _support;
-  }
+   /**
+    * Adds a feature index to the elements set.
+    *
+    * @param fte int An index of a feature that is part of this pattern
+    */
+   public void addPatternElt(int fte) { _patternElts.add(fte); }
 
-  public void setSupport(int s){
-    _support = s;
-  }
+   /**
+    * Adds features indices to the elements set.
+    *
+    * @param col int[] feature indices that are part of this pattern.
+    */
+   public void addPatternElts(int[] col) { _patternElts.addAll(col); }
 
-  public void addPatternElt(int fte){
-    _patternElts.add(fte);
-  }
+   /**
+    * Clears the set of the elements.
+    */
+   public void clearPatterns() { _patternElts.clear(); }
 
-  public void addPatternElts(int[] col){
-    _patternElts.addAll(col);
-  }
+   /**
+    * Constructs a new FPPAttern object and copies the value in this pattern to
+    * it. Returns the newly constructed object.
+    *
+    * @return FPPattern a copy of this FPPAttenr object.
+    */
+   public FPPattern copy() {
+      FPPattern newpat = new FPPattern();
+      newpat._support = this._support;
+      newpat._patternElts.addAll(_patternElts.toArray());
 
-  public void clearPatterns(){
-    _patternElts.clear();
-  }
-}
+      return newpat;
+   }
+
+   /**
+    * Returns an iterator to the pattern's elements.
+    *
+    * @return TIntIterator an iterator for the pattern's elements.
+    */
+   public TIntIterator getPattern() { return _patternElts.iterator(); }
+
+   /**
+    * Returns the size of the integers hash set - the number of attributes this
+    * pattenr contains.
+    *
+    * @return int The number of attributes this pattenr contains
+    */
+   public int getSize() { return _patternElts.size(); }
+
+   /**
+    * Returns the support for this pattenr.
+    *
+    * @return int The support for this pattenr.
+    */
+   public int getSupport() { return _support; }
+
+   /**
+    * Sets the support of this pattern.
+    *
+    * @param s int The value for the support of this pattern
+    */
+   public void setSupport(int s) { _support = s; }
+
+   /**
+    * A list like String representation of this pattern. For debug means
+    *
+    * @return String A list like String representation of this pattern.
+    */
+   public String toString() {
+      String retVal = "[ ";
+      TIntIterator it = this._patternElts.iterator();
+
+      while (it.hasNext()) {
+         int i = it.next();
+         retVal += i + ", ";
+      }
+
+      if (retVal.length() > 2) {
+         retVal = retVal.substring(0, retVal.length() - 2);
+      }
+
+      retVal += " ]";
+
+      return retVal;
+   }
+} // end class FPPattern
--- basic/src/ncsa/d2k/modules/core/discovery/ruleassociation/fpgrowth/FeatureTableElement.java
+++ basic/src/ncsa/d2k/modules/core/discovery/ruleassociation/fpgrowth/FeatureTableElement.java
@@ -1,69 +1,177 @@
+/*
+ * $Header$
+ *
+ * ===================================================================
+ *
+ * D2K-Workflow
+ * Copyright (c) 1997,2006 THE BOARD OF TRUSTEES OF THE UNIVERSITY OF
+ * ILLINOIS. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v2.0
+ * as published by the Free Software Foundation and with the required
+ * interpretation regarding derivative works as described below.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v2.0 for more details.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU General Public License v2.0 (GPL v2.0)
+ * which accompanies this distribution and is available at
+ * http://www.gnu.org/copyleft/gpl.html (or via mail from the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.), with the special and mandatory
+ * interpretation that software only using the documented public
+ * Application Program Interfaces (APIs) of D2K-Workflow are not
+ * considered derivative works under the terms of the GPL v2.0.
+ * Specifically, software only calling the D2K-Workflow Itinerary
+ * execution and workflow module APIs are not derivative works.
+ * Further, the incorporation of published APIs of other
+ * independently developed components into D2K Workflow code
+ * allowing it to use those separately developed components does not
+ * make those components a derivative work of D2K-Workflow.
+ * (Examples of such independently developed components include for
+ * example, external databases or metadata and provenance stores).
+ *
+ * Note: A non-GPL commercially licensed version of contributions
+ * from the UNIVERSITY OF ILLINOIS may be available from the
+ * designated commercial licensee RiverGlass, Inc. located at
+ * (www.riverglassinc.com)
+ * ===================================================================
+ *
+ */
 package ncsa.d2k.modules.core.discovery.ruleassociation.fpgrowth;
 
-//==============
-// Java Imports
-//==============
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
+
+/**
+ * A representaiton of a feature column in a table. Has references to the data,
+ * the original index, label and count.
+ *
+ * @author  $Author$
+ * @version $Revision$, $Date$
+ */
 public class FeatureTableElement {
 
-  //==============
-  // Data Members
-  //==============
-  private int _lbl = -1;
-  private ArrayList _ptrs = new ArrayList();
-  private int _cnt = 0;
-  private int _pos = -1;
+   //~ Instance fields *********************************************************
 
-  //================
-  // Constructor(s)
-  //================
+   /**
+    * The sum of the data of this feature in the original table.
+    */
+   private int _cnt = 0;
 
-  public FeatureTableElement(int lbl, int cnt, int pos){
-    _lbl = lbl;
-    _cnt = cnt;
-    _pos = pos;
-  }
+   /** The feature's index if all features are ordered by frequency in the original table.*/
+   private int _lbl = -1;
 
-  public FeatureTableElement(int lbl, int cnt, int pos, Collection nodes){
-    _lbl = lbl;
-    _cnt = cnt;
-    _pos = pos;
-    if (nodes != null){
-      _ptrs = new ArrayList();
-      _ptrs.addAll(nodes);
-    }
-  }
+   /** The position of this feature column in the original table. */
+   private int _pos = -1;
 
-  //================
-  // Public Methods
-  //================
-  public void clearList(){
-    _ptrs.clear();
-    _ptrs = null;
-  }
+   /**
+    * An arrya list of <code>
+    * ncsa.d2k.modules.core.discovery.ruleassociation.fpgrowth.FPTreeNode</code>
+    * objects.
+    */
+   private ArrayList _ptrs = new ArrayList();
 
-  public int getCnt(){
-    return _cnt;
-  }
+   //~ Constructors ************************************************************
 
-  public List getPointers(){
-    return _ptrs;
-  }
+   /**
+    * Constructs a FeatureTableElement object with label, count and original
+    * position.
+    *
+    * @param lbl int The feature's index if all features are ordered by frequency in the original table.
+    * @param cnt int The sum of the data of this feature in the original table.
+    * @param pos int Theoriginal position of this feature in the original table
+    */
+   public FeatureTableElement(int lbl, int cnt, int pos) {
+      _lbl = lbl;
+      _cnt = cnt;
+      _pos = pos;
+   }
 
-  public Iterator getPointersIter(){
-    return _ptrs.iterator();
-  }
+   /**
+    * Constructs a FeatureTableElement object with label, count, original
+    * position and the data.
+    *
+    * @param lbl   int The feature's index if all features are ordered by frequency in the original table.
+    * @param cnt   int The sum of the data of this feature in the original table.
+    * @param pos   int Theoriginal position of this feature in the original
+    *              table
+    * @param nodes Collection The data elements. holds <code>
+    *              ncsa.d2k.modules.core.discovery.ruleassociation.fpgrowth.FPTreeNode</code>
+    *              objects
+    */
+   public FeatureTableElement(int lbl, int cnt, int pos, Collection nodes) {
+      _lbl = lbl;
+      _cnt = cnt;
+      _pos = pos;
 
-  public int getLabel(){
-    return _lbl;
-  }
+      if (nodes != null) {
+         _ptrs = new ArrayList();
+         _ptrs.addAll(nodes);
+      }
+   }
 
-  public void addPointer(FPTreeNode node){
-    _ptrs.add(node);
-  }
+   //~ Methods *****************************************************************
 
-  public int getPosition(){
-    return _pos;
-  }
-}
+   /**
+    * Adds <codE>node</code> to the list of data elements.
+    *
+    * @param node FPTreeNode A data elements to be added to the pointers list.
+    */
+   public void addPointer(FPTreeNode node) { _ptrs.add(node); }
+
+
+   /**
+    * Clears the array list with the data elements.
+    */
+   public void clearList() {
+      _ptrs.clear();
+      _ptrs = null;
+   }
+
+   /**
+    * Returns the sum of the data of this feature in the original table.
+    *
+    * @return int The sum of the data of this feature in the original table.
+    */
+   public int getCnt() { return _cnt; }
+
+
+   /**
+    * Returns the feature's index if all features are ordered by frequency in the original table.
+    *
+    * @return int The feature's index if all features are ordered by frequency in the original table.
+    */
+   public int getLabel() { return _lbl; }
+
+   /**
+    * Returns the collection with the data elements.
+    *
+    * @return List The data elements of this feature. Hlds <code>
+    *         ncsa.d2k.modules.core.discovery.ruleassociation.fpgrowth.FPTreeNode</code>
+    *         objects
+    */
+   public List getPointers() { return _ptrs; }
+
+
+   /**
+    * Returns an iterator over the list of the data elements.
+    *
+    * @return Iterator an iterator over the list of the data elements
+    */
+   public Iterator getPointersIter() { return _ptrs.iterator(); }
+
+   /**
+    * Returns the original index of this feature's column in the original table.
+    *
+    * @return int
+    */
+   public int getPosition() { return _pos; }
+} // end class FeatureTableElement
