545bb0a61a4c32c7673b8b913846a9a452da8d08
Unknown <unknown@illinois.edu>
2006-09-01 20:17:55 +0000

    This commit was manufactured by cvs2svn to create branch 'post412'.

--- core/src/ncsa/d2k/modules/core/datatype/table/WeightedPredictionExample.java
+++
@@ -1,7 +0,0 @@
-package ncsa.d2k.modules.core.datatype.table;
-
-public interface WeightedPredictionExample extends PredictionExample {
-
-    public double getWeight();
-    public void setWeight(double weight);
-}
